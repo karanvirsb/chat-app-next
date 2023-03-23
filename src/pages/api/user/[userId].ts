3cfd6fc27b2af34660c1db9a844b3157286686d8
karanvirsb <karanvirboparai123@gmail.com>
2023-03-23 12:03:45 -0400

    created userid handler

---
+++ src/pages/api/user/[userId].ts
@@ -0,0 +1,19 @@
+import { getUser } from "@/server/Features/user/use-cases";
+import { IUser } from "@/server/Features/user/user";
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ControllerReturn<IUser>>
+) {
+  const userId = req.query.userId as string;
+  if (req.method === "GET") {
+    const foundUser = await getUser(userId);
+    res.json({
+      body: foundUser,
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}

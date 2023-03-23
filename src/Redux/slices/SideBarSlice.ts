1cae90401e472c155a74a3f118224e6e62c12473
karanvirsb <karanvirboparai123@gmail.com>
2023-03-23 11:37:41 -0400

    renamed

--- src/Redux/slices/SideBarSlice.ts
+++ src/Redux/slices/SideBarSlice.ts
@@ -1,27 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 // Define a type for the slice state
-type SideBarState = {
-    open: boolean;
-}
+type sideBarState = {
+  open: boolean;
+};
 
 // Define the initial state using that type
-const initialState: SideBarState = {
-    open: true,
+const initialState: sideBarState = {
+  open: true,
 };
 
 export const sideBarSlice = createSlice({
-    name: "sideBar",
-    // `createSlice` will infer the state type from the `initialState` argument
-    initialState,
-    reducers: {
-        setSideBarOpen: (state) => {
-            state.open = true;
-        },
-        setSideBarClosed: (state) => {
-            state.open = false;
-        },
+  name: "sideBar",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setSideBarOpen: (state) => {
+      state.open = true;
     },
+    setSideBarClosed: (state) => {
+      state.open = false;
+    },
+  },
 });
 
 export const { setSideBarOpen, setSideBarClosed } = sideBarSlice.actions;

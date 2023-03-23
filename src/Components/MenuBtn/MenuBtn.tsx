ad5d21ae72d009c44f7ae21c9efcc6449a2979e5
karanvirsb <karanvirboparai123@gmail.com>
2023-03-23 11:37:27 -0400

    prettier

--- src/Components/MenuBtn/MenuBtn.tsx
+++ src/Components/MenuBtn/MenuBtn.tsx
@@ -1,39 +1,36 @@
+import { setSideBarClosed, setSideBarOpen } from "@/Redux/slices/sideBarSlice";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../Hooks/reduxHooks";
-import {
-    setSideBarClosed,
-    setSideBarOpen,
-} from "../../Redux/slices/sideBarSlice";
 
 function MenuBtn() {
-    const dispatch = useAppDispatch();
-    const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
-    return (
-        <button onClick={toggleSideBar}>
-            <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='w-6 h-6 m-md:hidden'
-            >
-                <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-                />
-            </svg>
-        </button>
-    );
+  const dispatch = useAppDispatch();
+  const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
+  return (
+    <button onClick={toggleSideBar}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 m-md:hidden"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </button>
+  );
 
-    function toggleSideBar() {
-        if (isSideBarOpen) {
-            dispatch(setSideBarClosed());
-        } else {
-            dispatch(setSideBarOpen());
-        }
+  function toggleSideBar() {
+    if (isSideBarOpen) {
+      dispatch(setSideBarClosed());
+    } else {
+      dispatch(setSideBarOpen());
     }
+  }
 }
 
 export default MenuBtn;

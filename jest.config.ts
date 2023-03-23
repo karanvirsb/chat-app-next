a087385342255308afae03f991411804e1be3b59
karanvirsb <karanvirboparai123@gmail.com>
2023-03-23 11:46:56 -0400

    added jest config

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  testEnvironment: "node",
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+    // "^.+\\.(ts|tsx)?$": "ts-jest",
+    // "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["**/node_modules/**"],
+};
+export default config;

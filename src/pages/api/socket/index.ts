abd4b3ce7118dd6e090f98476fd766d4b06c1d09
karanvirsb <karanvirboparai123@gmail.com>
2023-04-05 18:24:27 -0400

    added socket server

---
+++ src/pages/api/socket/index.ts
@@ -0,0 +1,25 @@
+import { Server, Socket } from "socket.io";
+import { Server as NetServer } from "http";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponseServerIO } from "@/types/next";
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server as any, { path: "/api/socket" });
+    io.on("disconnect", () => {});
+    io.on("connection", (socket) => {
+      console.log(socket.id);
+     
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default SocketHandler;

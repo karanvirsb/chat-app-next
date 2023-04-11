adf51c38169c26304fd6b0c65869415906190e95
karanvirsb <karanvirboparai123@gmail.com>
2023-04-11 17:40:10 -0400

    added handler for getting channels

---
+++ src/pages/api/groupChannel/all/[groupId].ts
@@ -0,0 +1,40 @@
+import { getChannelsByGroupId } from "@/server/Features/groupChannel/use-cases";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const headers: { [key: string]: string } = {
+    "Content-Type": "application/json",
+  };
+  if (req.method === "GET") {
+    await getChannelsByGroupIdRequest(req, res, headers);
+  }
+}
+
+async function getChannelsByGroupIdRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    const groupId = req.query.groupId as string;
+    const foundChannels = await getChannelsByGroupId(groupId);
+    res.json({
+      headers,
+      statusCode: 200,
+      body: foundChannels,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
+  }
+}

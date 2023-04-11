f499e5fdf11f744ba958d98b209c5944d118841e
karanvirsb <karanvirboparai123@gmail.com>
2023-04-11 17:18:24 -0400

    added api handler

---
+++ src/pages/api/group/user/[userId].ts
@@ -0,0 +1,40 @@
+import { getGroupsByUserId } from "@/server/Features/group/use-cases";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const headers: { [key: string]: string } = {
+    "Content-Type": "application/json",
+  };
+  if (req.method === "GET") {
+    await getGroupsByUserIdRequest(req, res, headers);
+  }
+}
+
+async function getGroupsByUserIdRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    const userId = req.query.userId as string;
+    const foundGroups = await getGroupsByUserId(userId);
+    res.json({
+      headers,
+      statusCode: 200,
+      body: foundGroups,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
+  }
+}

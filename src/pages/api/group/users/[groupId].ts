6970552ec69fa59cd3da339694aee9b8fbe04fc2
karanvirsb <karanvirboparai123@gmail.com>
2023-04-11 17:16:58 -0400

    added api handler for getting users by group id

---
+++ src/pages/api/group/users/[groupId].ts
@@ -0,0 +1,40 @@
+import { getUsersByGroupId } from "@/server/Features/group/use-cases";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const headers: { [key: string]: string } = {
+    "Content-Type": "application/json",
+  };
+  if (req.method === "GET") {
+    await getUsersByGroupIdRequest(req, res, headers);
+  }
+}
+
+async function getUsersByGroupIdRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    const groupId = req.query.groupId as string;
+    const users = await getUsersByGroupId(groupId);
+    res.json({
+      headers,
+      statusCode: 200,
+      body: users,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
+  }
+}

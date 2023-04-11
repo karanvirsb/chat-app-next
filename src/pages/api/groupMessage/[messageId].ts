6317da7bc9272bba9692bc742a57f4f91ae5d18a
karanvirsb <karanvirboparai123@gmail.com>
2023-04-11 17:43:43 -0400

    created handler for getting messages

---
+++ src/pages/api/groupMessage/[messageId].ts
@@ -0,0 +1,40 @@
+import { getMessageById } from "@/server/Features/groupMessage/use-cases";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const headers: { [key: string]: string } = {
+    "Content-Type": "application/json",
+  };
+  if (req.method === "GET") {
+    await getGroupMessage(req, res, headers);
+  }
+}
+
+async function getGroupMessage(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    const messageId = req.query.messageId as string;
+    const foundMessage = await getMessageById(messageId);
+    res.json({
+      headers,
+      statusCode: 200,
+      body: foundMessage,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
+  }
+}

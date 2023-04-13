edbdfd30144c4402280ec15d256982249d063b08
karanvirsb <karanvirboparai123@gmail.com>
2023-04-13 13:54:08 -0400

    commented out codes

--- src/Hooks/useGetSession.ts
+++ src/Hooks/useGetSession.ts
@@ -1,32 +1,34 @@
-import { useEffect, useState } from "react";
-import { useSessionContext } from "supertokens-auth-react/recipe/session";
+// import { useEffect, useState } from "react";
+// import { useSessionContext } from "supertokens-auth-react/recipe/session";
+// import { getToken } from "next-auth/jwt";
 
-type IUseGetSession = {
-    sessionInfo: {
-        doesSessionExist: boolean;
-        userId: string;
-        accessTokenPayload: any;
-    } | null;
-}
+// type IUseGetSession = {
+//   sessionInfo: {
+//     doesSessionExist: boolean;
+//     userId: string;
+//     accessTokenPayload: any;
+//   } | null;
+// };
 
-export default function useGetSession(): IUseGetSession {
-    const [sessionInfo, setSessionInfo] = useState<{
-        doesSessionExist: boolean;
-        userId: string;
-        accessTokenPayload: any;
-    } | null>(null);
-    const session = useSessionContext();
+// export default function useGetSession(): IUseGetSession {
+//   const [sessionInfo, setSessionInfo] = useState<{
+//     doesSessionExist: boolean;
+//     userId: string;
+//     accessTokenPayload: any;
+//   } | null>(null);
+//   const session = useSessionContext();
 
-    useEffect(() => {
-        if (!session.loading) {
-            const { doesSessionExist, userId, accessTokenPayload } = session;
-            setSessionInfo({
-                doesSessionExist,
-                userId,
-                accessTokenPayload,
-            });
-        }
-    }, [session, session.loading]);
+//   useEffect(() => {
+//     if (!session.loading) {
+//       const { doesSessionExist, userId, accessTokenPayload } = session;
+//       setSessionInfo({
+//         doesSessionExist,
+//         userId,
+//         accessTokenPayload,
+//       });
+//     }
+//   }, [session, session.loading]);
 
-    return { sessionInfo };
-}
+//   return { sessionInfo };
+// }
+export {};

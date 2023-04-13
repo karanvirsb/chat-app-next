fa795a344a13440a714c2c882276e6098e2be4c3
karanvirsb <karanvirboparai123@gmail.com>
2023-04-13 13:11:52 -0400

    prettier

--- src/server/Features/user/use-cases/getUser.ts
+++ src/server/Features/user/use-cases/getUser.ts
@@ -2,26 +2,26 @@ import { IUser } from "../user";
 import { IMakeUsersDb } from "../data-access/users-db";
 
 type props = {
-    usersDb: IMakeUsersDb["returnType"];
+  usersDb: IMakeUsersDb["returnType"];
 };
 
 type returnData = Promise<{
-    success: boolean;
-    data: IUser | undefined;
-    error: string;
+  success: boolean;
+  data: IUser | undefined;
+  error: string;
 }>;
 
 export interface IGetUserUseCase {
-    getUser: (userId: string) => Promise<returnData>;
+  getUser: (userId: string) => Promise<returnData>;
 }
 
 export default function makeGetUser({
-    usersDb,
+  usersDb,
 }: props): IGetUserUseCase["getUser"] {
-    return async function getUser(userId: string): Promise<returnData> {
-        if (!userId) {
-            throw new Error("UserId must be passed through");
-        }
-        return await usersDb.findById({ id: userId });
-    };
+  return async function getUser(userId: string): Promise<returnData> {
+    if (!userId) {
+      throw new Error("UserId must be passed through");
+    }
+    return await usersDb.findById({ id: userId });
+  };
 }

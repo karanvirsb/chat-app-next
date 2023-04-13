51a998556a45f911f0f4e74a642aef37a8128f62
karanvirsb <karanvirboparai123@gmail.com>
2023-04-13 13:11:33 -0400

    aded next auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,125 @@
+// import supertokens from "supertokens-node";
+// import { superTokensNextWrapper } from "supertokens-node/nextjs";
+// import { backendConfig } from "../../../../config/backendConfig";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import NextCors from "nextjs-cors";
+// import { middleware } from "supertokens-node/lib/build/framework/express";
+
+// supertokens.init(backendConfig());
+
+// export default async function superTokens(
+//   req: NextApiRequest & Request,
+//   res: NextApiResponse & Response
+// ) {
+//   // NOTE: We need CORS only if we are querying the APIs from a different origin
+//   await NextCors(req, res, {
+//     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//     allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+//   });
+
+//   await superTokensNextWrapper(
+//     async (next) => {
+//       // This is needed for production deployments with Vercel
+//       res.setHeader(
+//         "Cache-Control",
+//         "no-cache, no-store, max-age=0, must-revalidate"
+//       );
+//       await middleware()(req, res, next);
+//     },
+//     req,
+//     res
+//   );
+//   if (!res.writableEnded) {
+//     res.status(404).send("Not found");
+//   }
+// }
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextApiRequest, NextApiResponse } from "next";
+import z from "zod";
+import { compare, genSalt, hash } from "bcrypt";
+import { getUserByUsername } from "@/server/Features/user/GetUserByUsername";
+import { addUserUC } from "@/server/Features/user/AddUser";
+import makeId from "@/server/Utilities/id";
+
+export const authOptions = {
+  secret: process.env.AUTH_SECRET,
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const foundUser = await getUserByUsername({
+          username: credentials?.username ?? "",
+        });
+
+        if (!foundUser.data) {
+          return null;
+        }
+
+        const checkPassword = await compare(
+          credentials?.password ?? "",
+          foundUser.data.password
+        );
+
+        if (!checkPassword) return null;
+        // If no error and we have user data, return it
+        return { id: foundUser.data.userId, username: foundUser.data.username };
+      },
+    }),
+    // ...add more providers here
+  ],
+};
+
+const user = z.object({
+  username: z.string().min(4).max(25),
+  password: z.string().min(8).max(25),
+});
+
+async function registerUser(req: NextApiRequest, res: NextApiResponse) {
+  const { username, password } = req.body;
+
+  const result = user.safeParse({ username, password });
+
+  if (!result.success) {
+    res.status(400).json(result.error);
+    return;
+  }
+
+  const hashPassword = await hash(password, await genSalt());
+  const createdUser = await addUserUC({
+    username,
+    password: hashPassword,
+    status: "online",
+    userId: makeId.makeId(),
+  });
+
+  // res.redirect("/api/auth/signin");
+  if (createdUser.success && createdUser.data) {
+    res.status(200).json({ completedRegistration: true });
+    return;
+  }
+  res.status(400).json({ error: createdUser.error });
+}
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const { nextauth } = req.query;
+
+  if (nextauth?.includes("signup") && req.method === "POST") {
+    registerUser(req, res);
+  } else {
+    NextAuth(req, res, authOptions);
+  }
+};

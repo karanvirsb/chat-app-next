ffb34f5641a0ffa09a33fdf787ca9b236983aa09
karanvirsb <karanvirboparai123@gmail.com>
2023-04-13 12:09:53 -0400

    added password

--- src/server/Features/user/user.ts
+++ src/server/Features/user/user.ts
@@ -1,42 +1,44 @@
 type props = {
-    sanitizeText: (text: string) => string;
+  sanitizeText: (text: string) => string;
 };
 
 export interface IUser {
-    userId: string;
-    username: string;
-    status: string;
+  userId: string;
+  username: string;
+  status: string;
+  password: string;
 }
+
 export default function buildUser({ sanitizeText }: props) {
-    return function makeUser({ userId, username, status }: IUser) {
-        if (userId.length <= 10) {
-            throw new Error("User must have an Id greater than 10 characters");
-        }
-        let sanitizedText = sanitizeText(username).trim();
-        if (sanitizedText.length < 1) {
-            throw new Error("Username does not contain any valid characters");
-        }
+  return function makeUser({ userId, username, status }: IUser) {
+    if (userId.length <= 10) {
+      throw new Error("User must have an Id greater than 10 characters");
+    }
+    let sanitizedText = sanitizeText(username).trim();
+    if (sanitizedText.length < 1) {
+      throw new Error("Username does not contain any valid characters");
+    }
 
-        if (sanitizedText.length < 3 || sanitizedText.length >= 30) {
-            throw new Error(
-                "Username must be greater than 3 characters but less than 30"
-            );
-        }
+    if (sanitizedText.length < 3 || sanitizedText.length >= 30) {
+      throw new Error(
+        "Username must be greater than 3 characters but less than 30"
+      );
+    }
 
-        if (!status) {
-            throw new Error("Must have a valid status");
-        }
+    if (!status) {
+      throw new Error("Must have a valid status");
+    }
 
-        const deletedUsername = "Deleted :`(";
-        return Object.freeze({
-            getUserId: () => userId,
-            getUsername: () => sanitizedText,
-            getStatus: () => status,
-            markDeleted: () => {
-                sanitizedText = deletedUsername;
-                userId = "deleted-" + userId;
-            },
-            isDeleted: () => sanitizedText === deletedUsername,
-        });
-    };
+    const deletedUsername = "Deleted :`(";
+    return Object.freeze({
+      getUserId: () => userId,
+      getUsername: () => sanitizedText,
+      getStatus: () => status,
+      markDeleted: () => {
+        sanitizedText = deletedUsername;
+        userId = "deleted-" + userId;
+      },
+      isDeleted: () => sanitizedText === deletedUsername,
+    });
+  };
 }

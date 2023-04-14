90df00d2c45439fa1fb0b8eafe25d639864257b7
karanvirsb <karanvirboparai123@gmail.com>
2023-04-14 09:30:49 -0400

    added session checks

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Spinner from "./Spinner/Spinner";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/api/auth/signin");
+    },
+  });
+
+  if (status === "loading") {
+    return (
+      <div>
+        <Spinner></Spinner>
+      </div>
+    );
+  }
   return (
     <>
       <Sidebar></Sidebar>

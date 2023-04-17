d89e8d7f0aaad9e7319b5c054cba04d21460415c
karanvirsb <karanvirboparai123@gmail.com>
2023-04-17 13:44:42 -0400

    created index for channels

---
+++ src/pages/api/groupChannel/index.ts
@@ -0,0 +1,49 @@
+import { IGroupChannel } from "@/server/Features/groupChannel/groupChannel";
+import { createChannel } from "@/server/Features/groupChannel/use-cases";
+import id from "@/server/Utilities/id";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const headers: { [key: string]: string } = {
+    "Content-Type": "application/json",
+  };
+  if (req.method?.toUpperCase() === "POST") {
+    await createChannelRequest(req, res, headers);
+  }
+}
+
+async function createChannelRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    const { channelInfo }: { channelInfo: IGroupChannel } = req.body;
+    console.log(channelInfo);
+    const createdChannel = await createChannel({
+      channelId: channelInfo.channelId ?? id.makeId(),
+      channelName: channelInfo.channelName,
+      dateCreated: channelInfo.dateCreated ?? new Date(),
+      groupId: channelInfo.groupId,
+    });
+    console.log(createdChannel);
+    res.json({
+      headers,
+      statusCode: 200,
+      body: createdChannel,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
+  }
+}

0984f2ac1275466889d8fc0747a64a09ed78923d
karanvirsb <karanvirboparai123@gmail.com>
2023-04-17 13:44:58 -0400

    prettier

--- src/server/Features/groupChannel/use-cases/createChannel.ts
+++ src/server/Features/groupChannel/use-cases/createChannel.ts
@@ -3,61 +3,65 @@ import { IMakeChannelDb } from "../data-access/channel-db";
 import makeChannel from "../index";
 
 export type handleModerationType = {
-    (name: string): Promise<number | boolean>;
+  (name: string): Promise<number | boolean>;
 };
 
 type props = {
-    channelDb: IMakeChannelDb["returnType"];
-    handleModeration: handleModerationType;
+  channelDb: IMakeChannelDb["returnType"];
+  handleModeration: handleModerationType;
 };
 
 type returnData = Promise<{
-    success: boolean;
-    data: IGroupChannel | undefined;
-    error: string;
+  success: boolean;
+  data: IGroupChannel | undefined;
+  error: string;
 }>;
 
 export interface ICreateChannelUseCase {
-    createChannel: (channelInfo: IGroupChannel) => Promise<returnData>;
+  createChannel: (channelInfo: IGroupChannel) => Promise<returnData>;
 }
 
 export default function makeCreateChannel({
-    handleModeration,
-    channelDb,
+  handleModeration,
+  channelDb,
 }: props): ICreateChannelUseCase["createChannel"] {
-    return async function createChannel(
-        channelInfo: IGroupChannel
-    ): Promise<returnData> {
-        if (!channelInfo.channelName)
-            throw new Error("Channel name needs to be supplied");
-        if (!channelInfo.groupId)
-            throw new Error("Group Id needs to be supplied");
+  return async function createChannel(
+    channelInfo: IGroupChannel
+  ): Promise<returnData> {
+    if (!channelInfo.channelName)
+      throw new Error("Channel name needs to be supplied");
+    if (!channelInfo.groupId) throw new Error("Group Id needs to be supplied");
 
-        const channel = makeChannel(channelInfo);
+    const channel = makeChannel(channelInfo);
+    console.log("🚀 ~ file: createChannel.ts:37 ~ channel:", channel);
 
-        const moderatedName = await handleModeration(channel.getChannelName());
+    const moderatedName = await handleModeration(channel.getChannelName());
+    console.log(
+      "🚀 ~ file: createChannel.ts:40 ~ moderatedName:",
+      moderatedName
+    );
 
-        if (moderatedName) {
-            throw new Error("Channel name contains profanity");
-        }
+    if (moderatedName) {
+      throw new Error("Channel name contains profanity");
+    }
 
-        if (moderatedName === -1) {
-            throw new Error("Server Error, please try again.");
-        }
+    if (moderatedName === -1) {
+      throw new Error("Server Error, please try again.");
+    }
 
-        const channelExists = await channelDb.getChannelById(
-            channel.getChannelId()
-        );
+    const channelExists = await channelDb.getChannelById(
+      channel.getChannelId()
+    );
 
-        if (channelExists.success && channelExists.data !== undefined) {
-            throw new Error("Channel already exists, try again.");
-        }
+    if (channelExists.success && channelExists.data !== undefined) {
+      throw new Error("Channel already exists, try again.");
+    }
 
-        return await channelDb.createChannel({
-            channelId: channel.getChannelId(),
-            channelName: channel.getChannelName(),
-            dateCreated: channel.getDateCreated(),
-            groupId: channel.getGroupId(),
-        });
-    };
+    return await channelDb.createChannel({
+      channelId: channel.getChannelId(),
+      channelName: channel.getChannelName(),
+      dateCreated: channel.getDateCreated(),
+      groupId: channel.getGroupId(),
+    });
+  };
 }

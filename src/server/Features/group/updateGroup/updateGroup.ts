8122c66eddade464380043d898c58f6c5bfdfba2
karanvirsb <karanvirboparai123@gmail.com>
2023-04-19 12:11:01 -0400

    created update group use case

--- src/server/Features/group/updateGroup/updateGroup.ts
+++ src/server/Features/group/updateGroup/updateGroup.ts
@@ -10,6 +10,28 @@ const UpdateGroupSchema = z.object({
 
 type UpdateGroup = z.infer<typeof UpdateGroupSchema>;
 
+export function makeUpdateGroupUC({
+  updateGroupDBA,
+}: {
+  updateGroupDBA: ({
+    groupId,
+    updates,
+  }: UpdateGroup) => Promise<DBAccessReturn<IGroup>>;
+}) {
+  return async function updateGroupUC({
+    groupId,
+    updates,
+  }: UpdateGroup): Promise<UseCaseReturn<IGroup>> {
+    const result = UpdateGroupSchema.safeParse({ groupId, updates });
+
+    if (!result.success) {
+      return { success: false, error: result.error };
+    }
+
+    return await updateGroupDBA({ groupId, updates });
+  };
+}
+
 export function makeUpdateGroupDBA({ makeDb }: { makeDb: IGroupDb["makeDb"] }) {
   return async function updateGroupDBA({
     groupId,

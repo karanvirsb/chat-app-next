238a74088d31e9f2c0dc3dc19018277fe1d68d99
karanvirsb <karanvirboparai123@gmail.com>
2023-04-19 10:19:24 -0400

    added group side bar info

--- src/pages/group/[...groupId].tsx
+++ src/pages/group/[...groupId].tsx
@@ -5,34 +5,43 @@ import GroupChat from "./Components/GroupChat";
 import GroupTopBar from "./Components/GroupTopBar";
 import GroupUsers from "./Components/GroupUsers";
 import { useSearchParams } from "next/navigation";
+import { useAppSelector } from "@/Hooks/reduxHooks";
+import GroupSidebarInfo from "./Components/GroupSidebarInfo";
 
 export default function GroupChannel() {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(true);
   const searchParams = useSearchParams();
   const groupId = searchParams.get("groupId") ?? "";
-  const [isUserMenuOpen, setIsUserMenuOpen] = useState(true);
-
+  const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
   return (
-    <ChannelContainer>
-      <>
-        <GroupTopBar
-          isUserMenuOpen={isUserMenuOpen}
-          toggleUserMenu={toggleUserMenu}
-          groupId={groupId}
-        ></GroupTopBar>
-        <ScrollWrapper>
-          <GroupChat groupId={groupId}></GroupChat>
-          <>
-            {isUserMenuOpen ? (
-              <GroupUsers
-                isUserMenuOpen={isUserMenuOpen}
-                toggleUserMenu={toggleUserMenu}
-                groupId={groupId}
-              ></GroupUsers>
-            ) : null}
-          </>
-        </ScrollWrapper>
-      </>
-    </ChannelContainer>
+    <>
+      {isSideBarOpen && (
+        <GroupSidebarInfo
+          groupId={searchParams.get("groupId") ?? ""}
+        ></GroupSidebarInfo>
+      )}
+      <ChannelContainer>
+        <>
+          <GroupTopBar
+            isUserMenuOpen={isUserMenuOpen}
+            toggleUserMenu={toggleUserMenu}
+            groupId={groupId}
+          ></GroupTopBar>
+          <ScrollWrapper>
+            <GroupChat groupId={groupId}></GroupChat>
+            <>
+              {isUserMenuOpen ? (
+                <GroupUsers
+                  isUserMenuOpen={isUserMenuOpen}
+                  toggleUserMenu={toggleUserMenu}
+                  groupId={groupId}
+                ></GroupUsers>
+              ) : null}
+            </>
+          </ScrollWrapper>
+        </>
+      </ChannelContainer>
+    </>
   );
   function toggleUserMenu() {
     setIsUserMenuOpen((prev) => !prev);

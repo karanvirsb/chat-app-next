98c8c250f1b28b1e0afc44de2c39482c568c87dd
karanvirsb <karanvirboparai123@gmail.com>
2023-04-23 15:04:58 -0400

    added channels

--- src/Redux/slices/groupSlice.ts
+++ src/Redux/slices/groupSlice.ts
@@ -1,13 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { IGroup } from "@/server/Features/group/group";
+import { IGroupChannel } from "@/server/Features/groupChannel/groupChannel";
 
 interface IGroupState {
   groups: IGroup[];
+  channels: IGroupChannel[];
 }
 
 const initialState: IGroupState = {
   groups: [],
+  channels: [],
 };
 
 const groupSlice = createSlice({
@@ -17,8 +20,14 @@ const groupSlice = createSlice({
     setGroups(state, action: PayloadAction<IGroup[]>) {
       state.groups = action.payload;
     },
-    setGroup(state, action: PayloadAction<IGroup>) {
+    addGroup(state, action: PayloadAction<IGroup>) {
       state.groups.push(action.payload);
+    },
+    setChannels(state, action: PayloadAction<IGroupChannel[]>) {
+      state.channels = action.payload;
+    },
+    addChannel(state, action: PayloadAction<IGroupChannel>) {
+      state.channels.push(action.payload);
     },
   },
 });

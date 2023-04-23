86c39de8ef49a7ee6cb134ca7018b79a1f586c40
karanvirsb <karanvirboparai123@gmail.com>
2023-04-22 21:35:14 -0400

    updated query

--- src/server/Features/user/db/index.ts
+++ src/server/Features/user/db/index.ts
@@ -7,9 +7,10 @@ export default async function setupUserDb() {
   const db = await makeDb();
   const result = await db.query(`
     CREATE TABLE IF NOT EXISTS usert (
-      "userId" VARCHAR(100) REFERENCES emailpassword_users(user_id) ON DELETE CASCADE ON UPDATE CASCADE PRIMARY KEY,
+      "userId" VARCHAR(100) PRIMARY KEY ON DELETE CASCADE ON UPDATE CASCADE ,
       username VARCHAR(50) UNIQUE, 
-      status VARCHAR(50));
+      status VARCHAR(50),
+      password VARCHAR(256));
   `);
   // console.log("🚀 ~ file: index.ts ~ line 24 ~ setupUserDb ~ result", result);
   console.log("User Database set up complete...", result);

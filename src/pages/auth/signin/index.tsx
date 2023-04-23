722171483f9dafdcc2ad4e7fd70069c0b3a3987d
karanvirsb <karanvirboparai123@gmail.com>
2023-04-22 20:34:19 -0400

    updated ui

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -3,23 +3,80 @@ import type {
   InferGetServerSidePropsType,
 } from "next";
 import { getCsrfToken } from "next-auth/react";
+import Image from "next/image";
 
 export default function SignIn({
   csrfToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <form method="post" action="/api/auth/callback/credentials">
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        Username
-        <input name="username" type="text" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
+    <main className="flex flex-col justify-center items-center w-full h-screen">
+      <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex justify-center mx-auto">
+          <Image
+            className=""
+            width={50}
+            height={50}
+            src="/images/logo-nobg.svg"
+            alt="Chatter"
+          />
+        </div>
+
+        <form
+          className="mt-6"
+          method="post"
+          action="/api/auth/callback/credentials"
+        >
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm text-gray-800 dark:text-gray-200"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm text-gray-800 dark:text-gray-200"
+              >
+                Password
+              </label>
+            </div>
+
+            <input
+              type="password"
+              name="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-6">
+            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              Sign In
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-8 text-xs font-light text-center text-gray-400">
+          {" "}
+          Don't have an account?{" "}
+          <a
+            href="#"
+            className="font-medium text-gray-700 dark:text-gray-200 hover:underline"
+          >
+            Create One
+          </a>
+        </p>
+      </div>
+    </main>
   );
 }
 

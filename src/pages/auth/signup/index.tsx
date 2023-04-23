36c10ca2efd81eacfbfb0d99979659479fa42b4b
karanvirsb <karanvirboparai123@gmail.com>
2023-04-22 21:07:36 -0400

    updated ui

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -4,47 +4,104 @@ import React from "react";
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../api/auth/[...nextauth]";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Signup({
   csrfToken,
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
   return (
-    <>
-      {Object.values(providers).map(
-        (provider) =>
-          provider.name.toLowerCase() !== "credentials" && (
-            <div key={provider.name}>
-              <button onClick={() => signIn(provider.id)}>
-                Sign up with {provider.name}
-              </button>
+    <main className="flex flex-col justify-center items-center w-full h-screen">
+      <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="flex justify-center mx-auto">
+          <Image
+            className=""
+            width={50}
+            height={50}
+            src="/images/logo-nobg.svg"
+            alt="Chatter"
+          />
+        </div>
+
+        <form className="mt-6" method="post" onSubmit={handleSubmit}>
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm text-gray-800 dark:text-gray-200"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-4">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm text-gray-800 dark:text-gray-200"
+              >
+                Password
+              </label>
             </div>
-          )
-      )}
-      <form
-        method="post"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const result = await fetch("/api/auth/signup", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username: "John", password: "John_123" }),
-          });
-        }}
-      >
-        {/* <input name="csrfToken" type="hidden" defaultValue={csrfToken} /> */}
-        <label>
-          Username
-          <input name="username" type="text" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-    </>
+
+            <input
+              type="password"
+              name="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mt-6">
+            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+              Sign Up
+            </button>
+          </div>
+        </form>
+        {Object.values(providers).map(
+          (provider) =>
+            provider.name.toLowerCase() !== "credentials" && (
+              <div key={provider.name}>
+                <button onClick={() => signIn(provider.id)}>
+                  Sign up with {provider.name}
+                </button>
+              </div>
+            )
+        )}
+        <p className="mt-8 text-xs font-light text-center text-gray-400">
+          <a
+            href="/auth/signin"
+            className="font-medium text-gray-700 dark:text-gray-200 hover:underline"
+          >
+            Already have an account?
+          </a>
+        </p>
+      </div>
+    </main>
   );
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const result = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: "John",
+        password: "John_123",
+      }),
+    });
+    const data: { completedRegistration: boolean } = await result.json();
+    console.log(data);
+    if (result.status === 200 && data.completedRegistration) {
+      router.push("/auth/signin");
+    }
+  }
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {

3cb66ba92a302ed4add8daf20a086a982970ccdd
karanvirsb <karanvirboparai123@gmail.com>
2023-04-23 12:40:58 -0400

    added group reducer

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./slices/modalSlice";
 import sideBarReducer from "@/Redux/slices/sideBarSlice";
+import { groupReducer } from "./slices/groupSlice";
 
 export const store = configureStore({
   reducer: {
     modalReducer,
     sideBarReducer,
+    groupReducer,
   },
 });
 

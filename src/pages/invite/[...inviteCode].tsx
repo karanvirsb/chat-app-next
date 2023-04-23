a6da789b71b1a0da70f9dac3d1c1a5f08d5b75fe
karanvirsb <karanvirboparai123@gmail.com>
2023-04-23 08:49:53 -0400

    created invite code

---
+++ src/pages/invite/[...inviteCode].tsx
@@ -0,0 +1,35 @@
+import Spinner from "@/Components/Spinner/Spinner";
+import { useAppDispatch } from "@/Hooks/reduxHooks";
+import { setModal } from "@/Redux/slices/modalSlice";
+import { useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+
+function Invite() {
+  const { data: sessionInfo } = useSession();
+  const params = useSearchParams();
+  const dispatch = useAppDispatch();
+  // get the location from
+  const router = useRouter();
+  // if user is logged in send the invite code to the modal
+  useEffect(() => {
+    if (sessionInfo?.user) {
+      router.push(`/me`); // either navigate home and send open modal for join group
+      dispatch(
+        setModal({
+          open: true,
+          modalName: "joinGroup",
+          options: { inviteCode: params.get("inviteCode") ?? "" },
+        })
+      );
+    }
+  }, [sessionInfo]);
+  // if user is not logged in log in and then send to invite code modal
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <Spinner></Spinner>
+    </div>
+  );
+}
+
+export default Invite;

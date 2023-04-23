d68c06931e651c29344de5256ff8af5f47b7cf01
karanvirsb <karanvirboparai123@gmail.com>
2023-04-23 09:19:09 -0400

    added create group user

--- src/pages/api/groupUsers/index.ts
+++ src/pages/api/groupUsers/index.ts
@@ -1,4 +1,5 @@
 import {
+  createGroupUserUC,
   deleteGroupUserUC,
   updateGroupUserUC,
 } from "@/server/Features/groupUsers/slice";
@@ -11,10 +12,45 @@ export default async function handler(
   const headers: { [key: string]: string } = {
     "Content-Type": "application/json",
   };
-  if (req.method?.toUpperCase() === "DELETE") {
+  if (req.method?.toUpperCase() === "POST") {
+    await createGroupUserRequest(req, res, headers);
+  } else if (req.method?.toUpperCase() === "DELETE") {
     await deleteGroupUserRequest(req, res, headers);
   } else if (req.method?.toUpperCase() === "PUT") {
     await updateGroupUserRequest(req, res, headers);
+  }
+}
+
+async function createGroupUserRequest(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+  headers: { [key: string]: string }
+) {
+  try {
+    console.log(req.body);
+    const gId = req.body.groupId as string;
+    const uId = req.body.userId as string;
+    const createdGroupUser = await createGroupUserUC({
+      gId,
+      uId,
+      lastChecked: new Date(),
+      roles: ["2001"],
+    });
+    res.json({
+      headers,
+      statusCode: 200,
+      body: createdGroupUser,
+    });
+  } catch (error: any) {
+    res.json({
+      headers,
+      statusCode: 400,
+      body: {
+        success: false,
+        data: undefined,
+        error: error.message,
+      },
+    });
   }
 }
 

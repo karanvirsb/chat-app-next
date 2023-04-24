af4bf9649f3e0e3ce573cb67837d9479e9ee8616
karanvirsb <karanvirboparai123@gmail.com>
2023-04-24 11:36:51 -0400

    removed reconnection attempts

--- src/Sockets/index.ts
+++ src/Sockets/index.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:3000", {
-  reconnectionAttempts: 2,
   closeOnBeforeunload: false,
   autoConnect: true,
   path: "/api/socket",

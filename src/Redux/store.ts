92ebf6c090fd84310f17de16ee47c4f363c9d6e7
karanvirsb <karanvirboparai123@gmail.com>
2023-04-24 11:37:40 -0400

    added socket reducer and middleware

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -5,15 +5,18 @@ import sideBarReducer from "@/Redux/slices/sideBarSlice";
 import { groupMiddleware } from "./group/groupMiddleware";
 import { groupReducer } from "./group/groupSlice";
 import modalReducer from "./slices/modalSlice";
+import { socketMiddleware } from "./socket/socketMiddleware";
+import socketReducer from "./socket/socketSlice";
 
 export const store = configureStore({
   reducer: {
+    socketReducer,
+    groupReducer,
     modalReducer,
     sideBarReducer,
-    groupReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({}).concat([groupMiddleware]),
+    getDefaultMiddleware({}).concat([socketMiddleware, groupMiddleware]),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself

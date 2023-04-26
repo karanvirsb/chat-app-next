aec8194bb19bf0ed784e3135c60995fb2b75b2c2
karanvirsb <karanvirboparai123@gmail.com>
2023-04-26 16:09:11 -0400

    create shared events

---
+++ src/shared/socket-events/groupEventTypes.ts
@@ -0,0 +1,37 @@
+import { IGroup } from "@/server/Features/group/group";
+import { IUser } from "@/server/Features/user/user";
+
+export type InvalidateEvent = {
+  queryTags: string[];
+  groupId: string;
+};
+
+export type UpdateEvent = {
+  groupId: string;
+  payload: Partial<IGroup>;
+};
+
+export type DeleteEvent = {
+  groupId: string;
+  payload: {};
+};
+
+export type JoinRoomEvent = {
+  rooms: string[];
+  userId: string;
+};
+
+export type UpdateGroupUsersEvent = {
+  groupId: string;
+  payload: { userInfo: IUser };
+};
+
+export type LeaveRoomEvent = {
+  groupId: string;
+  payload: { userId: string };
+};
+
+export type LeaveGroupEvent = {
+  groupId: string;
+  payload: { userId: string };
+};

bf33374713446df490aa626eedb4a4b576123a84
karanvirsb <karanvirboparai123@gmail.com>
2023-04-26 19:19:19 -0400

    removed socket event

--- src/Redux/group/groupMiddleware.ts
+++ src/Redux/group/groupMiddleware.ts
@@ -2,11 +2,9 @@ import { Middleware } from "@reduxjs/toolkit";
 import { Session } from "next-auth";
 import { getSession } from "next-auth/react";
 
-import { IGroup } from "@/server/Features/group/group";
 import socket from "@/Sockets";
-import { UpdateEvent } from "@/Sockets/types/groupTypes";
 
-import { groupActions, IGroupState } from "./groupSlice";
+import { groupActions } from "./groupSlice";
 
 export const groupMiddleware: Middleware = (store) => (next) => {
   let session: Session | null;
@@ -17,21 +15,6 @@ export const groupMiddleware: Middleware = (store) => (next) => {
     if (groupActions.updateGroup.match(action) && socket.connected) {
       console.log(action);
       socket.emit("update_group", action.payload);
-      socket.on("updates_for_group", (data: UseCaseReturn<IGroup>) => {
-        if (data.success && data.data !== undefined) {
-          const groups: IGroupState["groups"] =
-            store.getState().groupReducer.groups;
-          const group = groups.findIndex(
-            (group) => group.groupId === data.data?.groupId
-          );
-          const newGroups: IGroupState["groups"] = [...groups];
-          if (group !== -1) {
-            newGroups[group] = data.data;
-          }
-          store.dispatch(groupActions.setGroups(newGroups));
-        }
-        socket.off("updates_for_group");
-      });
     }
 
     if (groupActions.setGroups.match(action) && socket.connected) {

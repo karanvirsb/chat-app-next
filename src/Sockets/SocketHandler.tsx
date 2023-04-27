5adaa51f1fc9fb2695d771a24676f3abca7316a2
karanvirsb <karanvirboparai123@gmail.com>
2023-04-26 20:01:10 -0400

    updated moved to group chat events

--- src/Sockets/SocketHandler.tsx
+++ src/Sockets/SocketHandler.tsx
@@ -8,6 +8,7 @@ import { IMessage } from "../Hooks/groupChatHooks";
 import { IGroup, IGroupUsers, IUser } from "../Hooks/groupHooks";
 import { PaginatedGroupMessages } from "../utilities/types/pagination";
 import socket from ".";
+import { groupChatEvents } from "./events/group-chat-events";
 import { groupEvents } from "./events/group-events";
 import { userEvents } from "./events/user-events";
 import { UpdateChannelsListEvent } from "./types/groupChannelTypes";
@@ -89,76 +90,7 @@ export default function SocketHandler({ children }: props) {
     });
 
     //Group chat events
-    socket.on("new_group_chat_message", (data: ICreateGroupMessageEvent) => {
-      const newMessage = data.payload.messageInfo;
-      const FIRST_PAGE = 0;
-      queryClient.setQueryData(
-        [`group-messages-${newMessage.channelId}`],
-        (oldData: unknown) => {
-          const pushResult = (
-            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-          ) => {
-            if (data.payload !== undefined) {
-              const updatedData = produce(infiniteData, (draft) => {
-                draft.pages[FIRST_PAGE].data.push(data.payload.messageInfo);
-              });
-              return updatedData;
-            }
-          };
-
-          return checkIfPagesExist(oldData) ? pushResult(oldData) : oldData;
-        }
-      );
-    });
-
-    socket.on("update_group_chat_message", (data: IUpdateGroupMessageEvent) => {
-      const updatedMessage = data.payload.messageInfo;
-      queryClient.setQueriesData(
-        [`group-messages-${updatedMessage.channelId}`],
-        (oldData: unknown) => {
-          const updateResult = (
-            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-          ) => {
-            if (updatedMessage !== undefined) {
-              const updatedData = produce(infiniteData, (draft) => {
-                draft.pages[data.payload.pageIndex].data[
-                  data.payload.messageIndex
-                ] = data.payload.messageInfo;
-              });
-
-              return updatedData;
-            }
-          };
-
-          return checkIfPagesExist(oldData) ? updateResult(oldData) : oldData;
-        }
-      );
-    });
-
-    socket.on("delete_group_chat_message", (data: IDeleteGroupMessageEvent) => {
-      const payload = data.payload;
-      queryClient.setQueriesData(
-        [`group-messages-${payload.channelId}`],
-        (oldData: unknown) => {
-          const deleteResult = (
-            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-          ) => {
-            if (payload !== undefined) {
-              const updatedData = produce(infiniteData, (draft) => {
-                draft.pages[data.payload.pageIndex].data.splice(
-                  data.payload.messageIndex,
-                  1
-                );
-              });
-
-              return updatedData;
-            }
-          };
-
-          return checkIfPagesExist(oldData) ? deleteResult(oldData) : oldData;
-        }
-      );
-    });
+    groupChatEvents({ socket, queryClient });
 
     return () => {
       socket.removeAllListeners();
@@ -166,15 +98,4 @@ export default function SocketHandler({ children }: props) {
     };
   }, [queryClient]);
   return <>{children}</>;
-}
-
-function checkIfPagesExist(
-  arr: unknown | InfiniteData<PaginatedGroupMessages<IMessage>>
-): arr is InfiniteData<PaginatedGroupMessages<IMessage>> {
-  return (
-    (arr as InfiniteData<PaginatedGroupMessages<IMessage>>).pages !==
-      undefined &&
-    (arr as InfiniteData<PaginatedGroupMessages<IMessage>>).pageParams !==
-      undefined
-  );
 }

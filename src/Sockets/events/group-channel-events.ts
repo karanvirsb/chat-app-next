a1726ec9ec668aedb7a084cbdfb217410b3debd6
karanvirsb <karanvirboparai123@gmail.com>
2023-04-26 20:04:54 -0400

    created group channel events

---
+++ src/Sockets/events/group-channel-events.ts
@@ -0,0 +1,24 @@
+import { QueryClient } from "@tanstack/react-query";
+import { Socket } from "socket.io-client";
+
+import { IGroupChannel } from "@/server/Features/groupChannel/groupChannel";
+
+import { UpdateChannelsListEvent } from "../types/groupChannelTypes";
+
+type props = { socket: Socket; queryClient: QueryClient };
+export function groupChannelEvents({ socket, queryClient }: props) {
+  socket.on("update_channel_list", (data: UpdateChannelsListEvent) => {
+    queryClient.setQueriesData(
+      [`group-channels-${data.groupId}`],
+      (oldData: unknown) => {
+        // get the old data and push new result
+        // need to assign it a new reference so it refreshes
+        const pushNewChannel = (arr: IGroupChannel[]) => {
+          return [...arr, data.payload.channelInfo];
+        };
+        // if the oldData is an array then add the push new channel
+        return Array.isArray(oldData) ? pushNewChannel(oldData) : oldData;
+      }
+    );
+  });
+}

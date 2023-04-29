3ed0d8c10ff364ec5181b035baa89ba514f769ab
karanvirsb <karanvirboparai123@gmail.com>
2023-04-29 15:11:18 -0400

    updated types

--- src/Sockets/events/group-channel-events.ts
+++ src/Sockets/events/group-channel-events.ts
@@ -2,23 +2,27 @@ import { QueryClient } from "@tanstack/react-query";
 import { Socket } from "socket.io-client";
 
 import { IGroupChannel } from "@/server/Features/groupChannel/groupChannel";
-
-import { UpdateChannelsListEvent } from "../types/groupChannelTypes";
+import { TGroupChannelEvents } from "@/shared/socket-events/groupChannelTypes";
 
 type props = { socket: Socket; queryClient: QueryClient };
 export function groupChannelEvents({ socket, queryClient }: props) {
-  socket.on("add_new_channel", (data: UpdateChannelsListEvent) => {
-    queryClient.setQueriesData(
-      [`group-channels-${data.groupId}`],
-      (oldData: unknown) => {
-        // get the old data and push new result
-        // need to assign it a new reference so it refreshes
-        const pushNewChannel = (arr: IGroupChannel[]) => {
-          return [...arr, data.payload.channelInfo];
-        };
-        // if the oldData is an array then add the push new channel
-        return Array.isArray(oldData) ? pushNewChannel(oldData) : oldData;
+  socket.on(
+    TGroupChannelEvents.ADD_CHANNEL.broadcast,
+    (data: UseCaseReturn<IGroupChannel>) => {
+      if (data.success) {
+        queryClient.setQueriesData(
+          [`group-channels-${data?.data?.groupId}`],
+          (oldData: unknown) => {
+            // get the old data and push new result
+            // need to assign it a new reference so it refreshes
+            const pushNewChannel = (arr: IGroupChannel[]) => {
+              return [...arr, data?.data];
+            };
+            // if the oldData is an array then add the push new channel
+            return Array.isArray(oldData) ? pushNewChannel(oldData) : oldData;
+          }
+        );
       }
-    );
-  });
+    }
+  );
 }

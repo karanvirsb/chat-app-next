14c9b1bac3ae70b899825b52adc121e2669d3f38
karanvirsb <karanvirboparai123@gmail.com>
2023-04-29 11:52:37 -0400

    added zod check

--- src/server/Features/groupChannel/groupChannel.ts
+++ src/server/Features/groupChannel/groupChannel.ts
@@ -1,11 +1,22 @@
+import { z, ZodError } from "zod";
+
 import { IId } from "../../Utilities/id";
 
-export interface IGroupChannel {
-  channelId: string;
-  channelName: string;
-  dateCreated: Date;
-  groupId: string;
-}
+export const GroupChannelSchema = z.object({
+  channelId: z.string().uuid(),
+  channelName: z.string().refine(
+    (val) => {
+      if (val.length < 3 || val.length > 50) {
+        return false;
+      }
+    },
+    { message: "Channel name should be between 3 to 50 characters long" }
+  ),
+  dateCreated: z.date(),
+  groupId: z.string().uuid(),
+});
+
+export type IGroupChannel = z.infer<typeof GroupChannelSchema>;
 
 type props = {
   Id: IId;
@@ -21,25 +32,34 @@ export default function buildChannel({ Id, sanitizeText }: props) {
   }: IGroupChannel) {
     const sanitizedChannelName = sanitizeText(channelName);
 
-    if (sanitizedChannelName.length <= 1) {
-      throw new Error("Channel name should contain valid characters");
+    const result = GroupChannelSchema.safeParse({
+      channelId,
+      sanitizedChannelName,
+      groupId,
+      dateCreated,
+    });
+    if (!result.success) {
+      throw new ZodError(result.error.issues);
     }
+    // if (sanitizedChannelName.length <= 1) {
+    //   throw new Error("Channel name should contain valid characters");
+    // }
 
-    if (sanitizedChannelName.length < 3 || sanitizedChannelName.length > 50) {
-      throw new Error("Channel name should be between 3 to 50 characters long");
-    }
+    // if (sanitizedChannelName.length < 3 || sanitizedChannelName.length > 50) {
+    //   throw new Error("Channel name should be between 3 to 50 characters long");
+    // }
 
-    if (!groupId) {
-      throw new Error("Group Id needs to be supplied");
-    }
+    // if (!groupId) {
+    //   throw new Error("Group Id needs to be supplied");
+    // }
 
-    if (!dateCreated) {
-      throw new Error("Date needs to be supplied");
-    }
+    // if (!dateCreated) {
+    //   throw new Error("Date needs to be supplied");
+    // }
 
-    if (!channelId) {
-      throw new Error("Channel Id needs to be supplied");
-    }
+    // if (!channelId) {
+    //   throw new Error("Channel Id needs to be supplied");
+    // }
 
     // replace any ' with a '' to escape
     const updatedChannelName = sanitizedChannelName.replace(/'/g, "''");

ae016e6d03f9bd133879b15f91aa55c4af000c06
karanvirsb <karanvirboparai123@gmail.com>
2023-04-30 12:20:01 -0400

    added entity return

--- src/server/Features/group/group.ts
+++ src/server/Features/group/group.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+import { EntityReturn } from "@/shared/types/returns";
+
 import { IId } from "../../Utilities/id";
 import { IInviteCodeGenerator } from "../../Utilities/inviteCodeGenerator";
 
@@ -10,19 +12,14 @@ type props = {
 };
 
 export const GroupSchema = z.object({
-  groupName: z.string().min(3).max(50),
-  groupId: z.string().min(20),
+  groupName: z
+    .string()
+    .min(3, { message: "Group name must be at least 3 characters long" })
+    .max(50, { message: "Group name must be at most 50 characters long" }),
+  groupId: z.string().uuid(),
   inviteCode: z.string().min(7).max(10),
   dateCreated: z.date(),
 });
-
-// export interface IGroup {
-//   groupName: string;
-//   groupId: string;
-//   inviteCode: string;
-//   dateCreated: Date;
-//   // lastActive: Date
-// }
 
 export type IGroup = z.infer<typeof GroupSchema>;
 
@@ -36,37 +33,55 @@ export default function buildGroup({
     groupId = Id.makeId(),
     inviteCode = inviteCodeGenerator.makeInviteCode(),
     dateCreated = new Date(),
-  }: IGroup) {
+  }: IGroup): EntityReturn<
+    Readonly<{
+      getGroupName: () => string;
+      getGroupId: () => string;
+      getInviteCode: () => string;
+      getDateCreated: () => Date;
+    }>
+  > {
     const regex = /'/g;
     const sanitizedGroupName = sanitizeText(groupName);
-
-    if (sanitizedGroupName.length < 1) {
-      throw new Error("Group name must contain valid characters");
+    const result = GroupSchema.safeParse({
+      groupName: sanitizedGroupName,
+      groupId,
+      inviteCode,
+      dateCreated,
+    });
+    if (!result.success) {
+      return { success: false, error: result.error };
     }
+    // if (sanitizedGroupName.length < 1) {
+    //   throw new Error("Group name must contain valid characters");
+    // }
 
-    if (sanitizedGroupName.length < 3 || sanitizedGroupName.length > 50) {
-      throw new Error("Group name must be between 3 and 50 characters long");
-    }
+    // if (sanitizedGroupName.length < 3 || sanitizedGroupName.length > 50) {
+    //   throw new Error("Group name must be between 3 and 50 characters long");
+    // }
 
-    if (!groupId) {
-      throw new Error("Group requires an Id");
-    }
+    // if (!groupId) {
+    //   throw new Error("Group requires an Id");
+    // }
 
-    if (!inviteCode) {
-      throw new Error("Group requires an invite code");
-    }
+    // if (!inviteCode) {
+    //   throw new Error("Group requires an invite code");
+    // }
 
-    if (!dateCreated || Number.isNaN(dateCreated.getTime()))
-      throw new Error("Date Created needs to be supplied.");
+    // if (!dateCreated || Number.isNaN(dateCreated.getTime()))
+    //   throw new Error("Date Created needs to be supplied.");
 
     // replace any ' with a '' to escape
     const newGroupName = sanitizedGroupName.replace(regex, "''");
 
-    return Object.freeze({
-      getGroupName: () => newGroupName,
-      getGroupId: () => groupId,
-      getInviteCode: () => inviteCode,
-      getDateCreated: () => dateCreated,
-    });
+    return {
+      success: true,
+      data: Object.freeze({
+        getGroupName: () => newGroupName,
+        getGroupId: () => groupId,
+        getInviteCode: () => inviteCode,
+        getDateCreated: () => dateCreated,
+      }),
+    };
   };
 }

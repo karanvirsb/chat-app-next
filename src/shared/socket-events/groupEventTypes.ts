4a4078c4b2df5cad792bcc64db137e9f57f44840
karanvirsb <karanvirboparai123@gmail.com>
2023-04-30 11:12:36 -0400

    created group event names

--- src/shared/socket-events/groupEventTypes.ts
+++ src/shared/socket-events/groupEventTypes.ts
@@ -1,6 +1,17 @@
 import { IGroup } from "@/server/Features/group/group";
 import { IUser } from "@/server/Features/user/user";
 
+export const GroupEventsNames = {
+  DELETE_GROUP: {
+    send: "delete_the_group",
+    broadcast: "delete_group",
+    error: "delete_group_error",
+  },
+};
+export type DeleteEvent = {
+  groupId: string;
+};
+
 export type InvalidateEvent = {
   queryTags: string[];
   groupId: string;
@@ -9,11 +20,6 @@ export type InvalidateEvent = {
 export type UpdateEvent = {
   groupId: string;
   group_updates: Partial<IGroup>;
-};
-
-export type DeleteEvent = {
-  groupId: string;
-  payload: {};
 };
 
 export type JoinRoomEvent = {

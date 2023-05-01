928f7f979aea88a9d279d87f08eb01bb98b0483c
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 11:02:40 -0400

    created context menu

---
+++ src/Components/ContextMenu/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type props = {
+  channelId: string;
+};
+// TODO when component is not clicked, it disappears
+
+export function ContextMenu({ channelId }: props) {
+  return (
+    <ul
+      className={`menu absolute left-0 top-0 bg-base-100 w-56 p-2 rounded-box`}
+    >
+      <li>
+        <button className="btn bg-btn-mutations border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-mutations-hover">
+          Delete
+        </button>
+      </li>
+    </ul>
+  );
+}

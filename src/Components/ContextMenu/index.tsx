e06fbd7ec1535813cbfe8fe462adee9ab9ede205
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 11:06:29 -0400

    added left of 100%

--- src/Components/ContextMenu/index.tsx
+++ src/Components/ContextMenu/index.tsx
@@ -8,7 +8,7 @@ type props = {
 export function ContextMenu({ channelId }: props) {
   return (
     <ul
-      className={`menu absolute left-0 top-0 bg-base-100 w-56 p-2 rounded-box`}
+      className={`menu absolute left-[100%] top-0 bg-base-100 w-56 p-2 rounded-box`}
     >
       <li>
         <button className="btn bg-btn-mutations border-none font-bold mb-2 text-btn-mutations-text hover:bg-btn-mutations-hover">

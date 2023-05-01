6f62e59769a91fb112479cc7fadd360d0aac0c13
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 13:53:36 -0400

    added delete channel

--- src/shared/socket-events/groupChannelTypes.ts
+++ src/shared/socket-events/groupChannelTypes.ts
@@ -4,4 +4,9 @@ export const TGroupChannelEvents = {
     broadcast: "add_new_channel",
     error: "add_new_channel_error",
   },
+  DELETE_CHANNEL: {
+    send: "delete_channel",
+    broadcast: "delete_the_channel",
+    error: "delete_the_channel_error",
+  },
 };

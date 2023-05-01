e309fd447a8ef95cd0590086bbc15b333b1659ac
karanvirsb <karanvirboparai123@gmail.com>
2023-05-01 13:29:44 -0400

    added is component visible

--- src/Components/ContextMenu/index.tsx
+++ src/Components/ContextMenu/index.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import useComponentVisible from "@/Hooks/useComponentVisible";
 
 type props = {
   children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 // TODO when component is not clicked, it disappears
 
-export function ContextMenu({ children }: props) {
+export function ContextMenu({ children, setIsOpen, isOpen }: props) {
+  const { isComponentVisible, ref, setIsComponentVisible } =
+    useComponentVisible({ initialIsVisible: isOpen });
+
+  useEffect(() => {
+    if (!isComponentVisible) {
+      setIsOpen(false);
+    }
+  }, [isComponentVisible, setIsOpen]);
+
+  useEffect(() => {
+    const handleEscPress = (event: KeyboardEvent) => {
+      if (event.code === "Escape") {
+        setIsComponentVisible(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleEscPress);
+    return () => {
+      document.removeEventListener("keydown", handleEscPress);
+    };
+  }, [setIsComponentVisible]);
+
   return (
     <ul
       className={`menu opacity-100 absolute left-[100%] top-0 bg-base-100  p-2 rounded-md`}
+      ref={ref}
     >
       {children}
       <li>

d4321e552bf0c31ece9326a929f8ac69427fbf3d
karanvirsb <karanvirboparai123@gmail.com>
2023-05-04 11:07:16 -0400

    created shared groupChatEventsTypes

---
+++ src/shared/socket-events/groupChatTypes.ts
@@ -0,0 +1,7 @@
+export const groupChatEventsTypes = {
+  NEW_MESSAGE: {
+    send: "create_group_message",
+    broadcast: "new_group_chat_message",
+    error: "new_group_chat_message_error",
+  },
+};

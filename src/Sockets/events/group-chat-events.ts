962711b2909a92014a26ba906191389c35ee1580
karanvirsb <karanvirboparai123@gmail.com>
2023-05-04 11:17:31 -0400

    added group events

--- src/Sockets/events/group-chat-events.ts
+++ src/Sockets/events/group-chat-events.ts
@@ -3,6 +3,7 @@ import produce from "immer";
 import { Socket } from "socket.io-client";
 
 import { IMessage } from "@/Hooks/groupChatHooks";
+import { groupChatEventsTypes } from "@/shared/socket-events/groupChatTypes";
 import { PaginatedGroupMessages } from "@/utilities/types/pagination";
 
 import {
@@ -14,84 +15,93 @@ import {
 type props = { socket: Socket; queryClient: QueryClient };
 
 export function groupChatEvents({ socket, queryClient }: props) {
-  socket.on("new_group_chat_message", (data: ICreateGroupMessageEvent) => {
-    const newMessage = data.payload.messageInfo;
-    const FIRST_PAGE = 0;
-    queryClient.setQueryData(
-      [`group-messages-${newMessage.channelId}`],
-      (oldData: unknown) => {
-        const pushResult = (
-          infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-        ) => {
-          if (data.payload !== undefined) {
-            const updatedData = produce(infiniteData, (draft) => {
-              if (draft?.pages[FIRST_PAGE] && draft.pages[FIRST_PAGE].data) {
-                draft.pages[FIRST_PAGE]?.data?.push(data.payload.messageInfo);
-              } else {
-                draft.pages[FIRST_PAGE] = {
-                  data: [data.payload.messageInfo],
-                  hasNextPage: false,
-                  nextPage: null,
-                };
-              }
-            });
-            return updatedData;
-          }
-        };
+  socket.on(
+    groupChatEventsTypes.NEW_MESSAGE.broadcast,
+    (data: ICreateGroupMessageEvent) => {
+      const newMessage = data.payload.messageInfo;
+      const FIRST_PAGE = 0;
+      queryClient.setQueryData(
+        [`group-messages-${newMessage.channelId}`],
+        (oldData: unknown) => {
+          const pushResult = (
+            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
+          ) => {
+            if (data.payload !== undefined) {
+              const updatedData = produce(infiniteData, (draft) => {
+                if (draft?.pages[FIRST_PAGE] && draft.pages[FIRST_PAGE].data) {
+                  draft.pages[FIRST_PAGE]?.data?.push(data.payload.messageInfo);
+                } else {
+                  draft.pages[FIRST_PAGE] = {
+                    data: [data.payload.messageInfo],
+                    hasNextPage: false,
+                    nextPage: null,
+                  };
+                }
+              });
+              return updatedData;
+            }
+          };
 
-        return checkIfPagesExist(oldData) ? pushResult(oldData) : oldData;
-      }
-    );
-  });
+          return checkIfPagesExist(oldData) ? pushResult(oldData) : oldData;
+        }
+      );
+    }
+  );
 
-  socket.on("update_group_chat_message", (data: IUpdateGroupMessageEvent) => {
-    const updatedMessage = data.payload.messageInfo;
-    queryClient.setQueriesData(
-      [`group-messages-${updatedMessage.channelId}`],
-      (oldData: unknown) => {
-        const updateResult = (
-          infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-        ) => {
-          if (updatedMessage !== undefined) {
-            const updatedData = produce(infiniteData, (draft) => {
-              draft.pages[data.payload.pageIndex].data[
-                data.payload.messageIndex
-              ] = data.payload.messageInfo;
-            });
+  socket.on(
+    groupChatEventsTypes.UPDATE_MESSAGE.broadcast,
+    (data: IUpdateGroupMessageEvent) => {
+      const updatedMessage = data.payload.messageInfo;
+      queryClient.setQueriesData(
+        [`group-messages-${updatedMessage.channelId}`],
+        (oldData: unknown) => {
+          const updateResult = (
+            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
+          ) => {
+            if (updatedMessage !== undefined) {
+              const updatedData = produce(infiniteData, (draft) => {
+                draft.pages[data.payload.pageIndex].data[
+                  data.payload.messageIndex
+                ] = data.payload.messageInfo;
+              });
 
-            return updatedData;
-          }
-        };
+              return updatedData;
+            }
+          };
 
-        return checkIfPagesExist(oldData) ? updateResult(oldData) : oldData;
-      }
-    );
-  });
+          return checkIfPagesExist(oldData) ? updateResult(oldData) : oldData;
+        }
+      );
+    }
+  );
 
-  socket.on("delete_group_chat_message", (data: IDeleteGroupMessageEvent) => {
-    const payload = data.payload;
-    queryClient.setQueriesData(
-      [`group-messages-${payload.channelId}`],
-      (oldData: unknown) => {
-        const deleteResult = (
-          infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
-        ) => {
-          if (payload !== undefined) {
-            const updatedData = produce(infiniteData, (draft) => {
-              draft.pages[data.payload.pageIndex].data.splice(
-                data.payload.messageIndex,
-                1
-              );
-            });
+  socket.on(
+    groupChatEventsTypes.DELETE_MESSAGE.broadcast,
+    (data: IDeleteGroupMessageEvent) => {
+      const payload = data.payload;
+      queryClient.setQueriesData(
+        [`group-messages-${payload.channelId}`],
+        (oldData: unknown) => {
+          const deleteResult = (
+            infiniteData: InfiniteData<PaginatedGroupMessages<IMessage>>
+          ) => {
+            if (payload !== undefined) {
+              const updatedData = produce(infiniteData, (draft) => {
+                draft.pages[data.payload.pageIndex].data.splice(
+                  data.payload.messageIndex,
+                  1
+                );
+              });
 
-            return updatedData;
-          }
-        };
+              return updatedData;
+            }
+          };
 
-        return checkIfPagesExist(oldData) ? deleteResult(oldData) : oldData;
-      }
-    );
-  });
+          return checkIfPagesExist(oldData) ? deleteResult(oldData) : oldData;
+        }
+      );
+    }
+  );
 }
 
 function checkIfPagesExist(

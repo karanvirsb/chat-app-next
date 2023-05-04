87c53f75938487a799c332b5df0943132960cb4f
karanvirsb <karanvirboparai123@gmail.com>
2023-05-04 11:08:19 -0400

    added update message events

--- src/shared/socket-events/groupChatTypes.ts
+++ src/shared/socket-events/groupChatTypes.ts
@@ -4,4 +4,9 @@ export const groupChatEventsTypes = {
     broadcast: "new_group_chat_message",
     error: "new_group_chat_message_error",
   },
+  UPDATE_MESSAGE: {
+    send: "update_group_message",
+    broadcast: "update_group_chat_message",
+    error: "update_group_chat_message_error",
+  },
 };

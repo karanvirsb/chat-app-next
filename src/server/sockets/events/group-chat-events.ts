cf731647f2e67f2700c9fa061b1a1fdec1c6859e
karanvirsb <karanvirboparai123@gmail.com>
2023-05-05 14:00:32 -0400

    added created message

--- src/server/sockets/events/group-chat-events.ts
+++ src/server/sockets/events/group-chat-events.ts
@@ -1,6 +1,7 @@
 import { Server, Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
+import { createMessage } from "@/server/Features/groupMessage/use-cases";
 import {
   groupChatEventDataTypes,
   groupChatEventsTypes,
@@ -14,11 +15,18 @@ type props = {
 export function groupChatEvents({ socket, io }: props) {
   socket.on(
     groupChatEventsTypes.NEW_MESSAGE.send,
-    (data: groupChatEventDataTypes["NEW_MESSAGE"]["send"]) => {
-      io.to(data.groupId).emit(
-        groupChatEventsTypes.NEW_MESSAGE.broadcast,
-        data
-      );
+    async (data: groupChatEventDataTypes["NEW_MESSAGE"]["send"]) => {
+      const createdMessage = await createMessage(data.payload.messageInfo);
+      if (createdMessage.success) {
+        io.to(data.groupId).emit(groupChatEventsTypes.NEW_MESSAGE.broadcast, {
+          ...data,
+          payload: { messageInfo: createdMessage.data },
+        });
+      } else {
+        io.to(data.groupId).emit(groupChatEventsTypes.NEW_MESSAGE.error, {
+          error: createdMessage.error,
+        });
+      }
     }
   );
 

355f6231288e066136c875905896bd32f41fbe96
karanvirsb <karanvirboparai123@gmail.com>
2023-05-10 11:42:31 -0400

    added message and display data arr

--- src/pages/group/Components/GroupChat.tsx
+++ src/pages/group/Components/GroupChat.tsx
@@ -1,10 +1,13 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 
 import { groupActions } from "@/Redux/group/groupSlice";
 import { IGroupMessage } from "@/server/Features/groupMessage/groupMessage";
+import { IUser } from "@/server/Features/user/user";
 
+import Message from "../../../Components/Messages/Message";
 import Messages from "../../../Components/Messages/Messages";
 import { useGetGroupMessagesByChannelIdQuery } from "../../../Hooks/groupChatHooks";
 import { useAppDispatch } from "../../../Hooks/reduxHooks";
@@ -20,15 +23,73 @@ export default function GroupChat({ groupId }: props): JSX.Element {
   const messageRef = useRef<null | HTMLInputElement>(null);
   const chatMessagesRef = useRef<null | HTMLDivElement>(null);
   // TODO after inital load need to set dateCreated to last message.
-  const { data: chatMessages, fetchNextPage } =
-    useGetGroupMessagesByChannelIdQuery({
-      channelId,
-      dateCreated: new Date().getTime(),
-      limit: 10,
-    });
+  const {
+    data: chatMessages,
+    fetchNextPage,
+    isSuccess,
+    isFetchingNextPage,
+  } = useGetGroupMessagesByChannelIdQuery({
+    channelId,
+    dateCreated: new Date().getTime(),
+    limit: 10,
+  });
 
   const dispatch = useAppDispatch();
   const { data: sessionInfo } = useSession();
+
+  const queryClient = useQueryClient();
+  const groupUsers = (
+    queryClient.getQueriesData([`group-users-${groupId}`])[0]
+      ? (queryClient.getQueriesData([
+          `group-users-${groupId}`,
+        ])[0][1] as unknown[])
+      : []
+  ) as IUser[];
+  console.log(groupUsers);
+  const foundUser = useCallback(
+    (id: string) => {
+      return groupUsers.find((user: IUser) => user.userId === id);
+    },
+    [groupUsers]
+  );
+  const displayMessages = useMemo(() => {
+    const newArr: React.ReactNode[] = [];
+    if (isSuccess && chatMessages !== undefined) {
+      chatMessages.pages.forEach((messages, pageIndex) => {
+        messages?.data.forEach((message, index) => {
+          const user = foundUser(message.userId);
+          newArr.push(
+            <Message
+              deleteCallback={handleDeletingMessage}
+              editCallback={handleEditMessage}
+              message={message}
+              messageIndex={index}
+              pageIndex={pageIndex}
+              username={user?.username ?? "Unknown"}
+              key={message.messageId}
+            ></Message>
+          );
+        });
+      });
+      // newArr.unshift(
+      //   <div ref={observerElem}>
+      //     <button
+      //       onClick={() => {
+      //         fetchNextPage();
+      //       }}
+      //       disabled={!hasNextPage || isFetchingNextPage}
+      //     >
+      //       {isFetchingNextPage
+      //         ? "Loading more..."
+      //         : hasNextPage
+      //         ? "Load More"
+      //         : "Nothing more to load"}
+      //     </button>
+      //   </div>
+      // );
+    }
+    return newArr;
+  }, [chatMessages, isSuccess]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -60,33 +121,34 @@ export default function GroupChat({ groupId }: props): JSX.Element {
             pressure!
           </p>
         ) : (
-          chatMessages?.pages.map((_, index, pages) => {
-            if (index === 0) {
-              return (
-                <Messages
-                  editCallback={handleEditMessage}
-                  deleteCallback={handleDeletingMessage}
-                  key={`messages-${index}`}
-                  groupId={groupId}
-                  messages={pages[pages.length - 1 - index]?.data}
-                  lastPage={true}
-                  fetchNextPage={fetchNextPage}
-                  pageIndex={pages.length - 1 - index}
-                ></Messages>
-              );
-            } else {
-              return (
-                <Messages
-                  editCallback={handleEditMessage}
-                  deleteCallback={handleDeletingMessage}
-                  key={`messages-${index}`}
-                  groupId={groupId}
-                  messages={pages[pages.length - 1 - index]?.data}
-                  pageIndex={pages.length - 1 - index}
-                ></Messages>
-              );
-            }
-          })
+          displayMessages.map((message) => message)
+          // chatMessages?.pages.map((_, index, pages) => {
+          //   if (index === 0) {
+          //     return (
+          //       <Messages
+          //         editCallback={handleEditMessage}
+          //         deleteCallback={handleDeletingMessage}
+          //         key={`messages-${index}`}
+          //         groupId={groupId}
+          //         messages={pages[pages.length - 1 - index]?.data}
+          //         lastPage={true}
+          //         fetchNextPage={fetchNextPage}
+          //         pageIndex={pages.length - 1 - index}
+          //       ></Messages>
+          //     );
+          //   } else {
+          //     return (
+          //       <Messages
+          //         editCallback={handleEditMessage}
+          //         deleteCallback={handleDeletingMessage}
+          //         key={`messages-${index}`}
+          //         groupId={groupId}
+          //         messages={pages[pages.length - 1 - index]?.data}
+          //         pageIndex={pages.length - 1 - index}
+          //       ></Messages>
+          //     );
+          //   }
+          // })
         )}
       </div>
       {channelId.length > 0 ? (

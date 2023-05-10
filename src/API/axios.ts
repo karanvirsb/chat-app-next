e114aeb37aa856df001fba2e1f33fbd4c821426e
karanvirsb <karanvirboparai123@gmail.com>
2023-05-10 12:39:09 -0400

    removed

--- src/API/axios.ts
+++ src/API/axios.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 import Session from "supertokens-auth-react/recipe/session";
-Session.addAxiosInterceptors(axios);
+// Session.addAxiosInterceptors(axios);
 export default axios;

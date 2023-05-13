1ccbffed655312a70ecece19ee090b944cae4b6e
karanvirsb <karanvirboparai123@gmail.com>
2023-05-12 23:05:21 -0400

    added forward ref

--- src/pages/group/Components/GroupTopBar.tsx
+++ src/pages/group/Components/GroupTopBar.tsx
@@ -1,67 +1,77 @@
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { MutableRefObject } from "react";
 
 import { useAppSelector } from "@/Hooks/reduxHooks";
 
 import ToggleSidebarBtn from "../../../Components/ToggleSidebarBtn/ToggleSidebarBtn";
-import TopBarContainer from "../../../Components/TopBarContainer/TopBarContainer";
+import { TopBarContainer } from "../../../Components/TopBarContainer/TopBarContainer";
 import toCapitalize from "../../../utilities/toCapitalize";
 
 type props = {
   isUserMenuOpen: boolean;
   toggleUserMenu: () => void;
+  // ref: MutableRefObject<HTMLDivElement | null>;
 };
 
-export default function GroupTopBar({ isUserMenuOpen, toggleUserMenu }: props) {
-  const searchParams = useSearchParams();
-  const selectedChannel = searchParams.get("channel") ?? "";
+export const GroupTopBar = React.forwardRef(
+  (
+    { isUserMenuOpen, toggleUserMenu }: props,
+    ref: React.ForwardedRef<HTMLDivElement | null>
+  ) => {
+    const searchParams = useSearchParams();
+    const selectedChannel = searchParams.get("channel") ?? "";
 
-  const channels = useAppSelector((state) => state.groupReducer.channels);
+    const channels = useAppSelector((state) => state.groupReducer.channels);
 
-  // filtering out the channel needed
-  const channel = channels.find((c) => c.channelId === selectedChannel);
+    // filtering out the channel needed
+    const channel = channels.find((c) => c.channelId === selectedChannel);
 
-  return (
-    <TopBarContainer>
-      <>
-        <div className="flex gap-4 items-center">
-          <ToggleSidebarBtn></ToggleSidebarBtn>
-          <span>{toCapitalize(channel?.channelName)}</span>
-        </div>
-        <button onClick={toggleUserMenu}>
-          {isUserMenuOpen ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 text-gray-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-              />
-            </svg>
-          )}
-        </button>
-      </>
-    </TopBarContainer>
-  );
-}
+    return (
+      <TopBarContainer ref={ref}>
+        <>
+          <div className="flex gap-4 items-center">
+            <ToggleSidebarBtn></ToggleSidebarBtn>
+            <span>{toCapitalize(channel?.channelName)}</span>
+          </div>
+          <button onClick={toggleUserMenu}>
+            {isUserMenuOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                />
+              </svg>
+            )}
+          </button>
+        </>
+      </TopBarContainer>
+    );
+  }
+);
+
+// export default function GroupTopBar({ isUserMenuOpen, toggleUserMenu }: props) {
+
+// }

dbadfdbc15d814f7ff72d26ac4ececedcf9d32af
karanvirsb <karanvirboparai123@gmail.com>
2023-05-13 15:19:49 -0400

    added overflow hidden

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -23,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     );
   }
   return (
-    <div className="flex h-[100dvh] max-h-[100dvh]">
+    <div className="flex h-[100dvh] max-h-[100dvh] overflow-hidden">
       <Sidebar></Sidebar>
       {/* <div className="min-w-[314px]"></div> */}
       {children}

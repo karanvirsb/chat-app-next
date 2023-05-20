856e958ece3a74ce2aeb81ac367dd84f24680443
karanvirsb <karanvirboparai123@gmail.com>
2023-05-20 16:45:49 -0400

    added extra pages

---
+++ src/pages/friends/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MenuBtn from "@/Components/MenuBtn/MenuBtn";
+
+export default function Friends() {
+  return (
+    <div>
+      <MenuBtn></MenuBtn>
+      Friends
+    </div>
+  );
+}
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MenuBtn from "@/Components/MenuBtn/MenuBtn";
+
+export default function Search() {
+  return (
+    <div>
+      <MenuBtn></MenuBtn>
+      Search
+    </div>
+  );
+}

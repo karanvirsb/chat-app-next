91fb63b6f85ed770637d091be0a62209cf7d8231
karanvirsb <karanvirboparai123@gmail.com>
2023-05-20 16:32:58 -0400

    added user input

---
+++ src/pages/settings/components/UserInput.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type props = {
+  labelName: string;
+  value: string;
+  onClick: (...args: any) => any | void;
+  inputType?: React.HTMLInputTypeAttribute | undefined;
+};
+
+export default function UserInput({
+  labelName,
+  value,
+  onClick,
+  inputType = "text",
+}: props) {
+  return (
+    <div className="form-control w-[90%]">
+      <label className="label">
+        <span className="label-text">{labelName}</span>
+      </label>
+      <div className="flex gap-4 w-full md:flex-row flex-col">
+        <input
+          type={inputType}
+          className="input input-bordered w-full"
+          contentEditable={false}
+          defaultValue={value}
+          readOnly={true}
+        />
+        <button
+          className="btn bg-btn-cta border-none text-black hover:bg-btn-cta-hover"
+          onClick={onClick}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+}

710e7a2fab34db7885beabba61c6bd01e68f1f86
karanvirsb <karanvirboparai123@gmail.com>
2023-05-20 16:48:06 -0400

    added me chat

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
+import ChannelContainer from "@/Components/ChannelContainer/ChannelContainer";
 import { Layout } from "@/Components/Layout";
+import { useAppSelector } from "@/Hooks/reduxHooks";
 
-function index() {
+import MeChat from "./Components/MeChat";
+import MeSideBarInfo from "./Components/MeSideBarInfo";
+import MeTopBar from "./Components/MeTopBar";
+
+export default function Me() {
+  const isSideBarOpen = useAppSelector((state) => state.sideBarReducer.open);
   return (
     <Layout>
-      <div>index</div>;
+      <>
+        {isSideBarOpen && <MeSideBarInfo></MeSideBarInfo>}
+        <ChannelContainer>
+          <>
+            <MeTopBar></MeTopBar>
+            <div className="flex flex-grow">
+              <MeChat></MeChat>
+            </div>
+          </>
+        </ChannelContainer>
+      </>
     </Layout>
   );
 }
-
-export default index;

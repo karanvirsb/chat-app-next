6faa9874211bd9207dc5ef11dbce032977f1ba58
karanvirsb <karanvirboparai123@gmail.com>
2023-05-21 11:12:36 -0400

    fixed and updated

--- src/server/Features/user/data-access/users-db.spec.ts
+++ src/server/Features/user/data-access/users-db.spec.ts
@@ -1,31 +1,31 @@
-import makeDb, { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import makeDb, { closeDb } from "@/server/__test__/fixures/db";
 import makeFakerUser from "@/server/__test__/fixures/user";
 import userTests from "@/server/__test__/functions/user";
 
+import { IUser } from "../user";
 import makeUsersDb, { IMakeUsersDb } from "./users-db";
 
 describe("Users DB", () => {
   let UsersDb: IMakeUsersDb["returnType"];
-
+  let user: IUser;
   beforeAll(async () => {
-    const createdUser = await userTests.addTestUserToDB({
-      userId: "1234",
-    });
+    user = await makeFakerUser({ userId: "1234" });
+    // await userTests.addTestUserToDB({
+    //   userId: "1234",
+    // });
     UsersDb = makeUsersDb({ makeDb });
-    // TODO await clearDb("usert");
   });
 
   afterEach(async () => {
-    const deletedUser = await userTests.deleteTestUser({ userId: "1234" });
+    await userTests.deleteTestUser({ userId: "1234" });
   });
 
   afterAll(async () => {
-    const deletedUser = await userTests.deleteTestUser({ userId: "1234" });
+    await userTests.deleteTestUser({ userId: "1234" });
     await closeDb();
   });
   it("Insert a user", async () => {
     // successful
-    const user = await makeFakerUser({ userId: "1234" });
     const resp = await UsersDb.insert({ data: user });
 
     if (resp.success) {
@@ -34,38 +34,35 @@ describe("Users DB", () => {
   });
   it("Find user by Id", async () => {
     //successful
-    const fakeUser = await makeFakerUser({ userId: "1234" });
-    const insertedUser = await UsersDb.insert({ data: fakeUser });
-    const resp = await UsersDb.findById({ id: fakeUser.userId });
+
+    await UsersDb.insert({ data: user });
+    const resp = await UsersDb.findById({ id: user.userId });
     if (resp.success) {
-      expect(resp.data?.username).toBe(fakeUser.username);
+      expect(resp.data?.username).toBe(user.username);
     }
   });
 
   it("Find user by username", async () => {
     // success
-    const fakeUser = await makeFakerUser({ userId: "1234" });
-    const insertedUser = await UsersDb.insert({ data: fakeUser });
-    const resp = await UsersDb.findByUsername(fakeUser.username);
+    await UsersDb.insert({ data: user });
+    const resp = await UsersDb.findByUsername(user.username);
     if (resp.success) {
-      expect(resp.data?.username).toBe(fakeUser.username);
+      expect(resp.data?.username).toBe(user.username);
     }
   });
 
   it("Remove user by Id", async () => {
-    const fakeUser = await makeFakerUser({ userId: "1234" });
-    const insertedUser = await UsersDb.insert({ data: fakeUser });
-    const resp = await UsersDb.remove(fakeUser.userId);
+    await UsersDb.insert({ data: user });
+    const resp = await UsersDb.remove(user.userId);
     if (resp.success) {
-      expect(resp.data?.username).toBe(fakeUser.username);
+      expect(resp.data?.username).toBe(user.username);
     }
   });
 
   it("Update user by username", async () => {
-    const fakeUser = await makeFakerUser({ userId: "1234" });
-    const insertedUser = await UsersDb.insert({ data: fakeUser });
+    await UsersDb.insert({ data: user });
     const resp = await UsersDb.updateByUsername({
-      username: fakeUser.username,
+      username: user.username,
       updates: {
         status: "offline",
       },
@@ -76,10 +73,9 @@ describe("Users DB", () => {
   });
 
   it("Update user by user id", async () => {
-    const fakeUser = await makeFakerUser({ userId: "1234" });
-    const insertedUser = await UsersDb.insert({ data: fakeUser });
+    await UsersDb.insert({ data: user });
     const resp = await UsersDb.updateByUserId({
-      userId: fakeUser.userId,
+      userId: user.userId,
       updates: {
         username: "johnB",
         status: "offline",

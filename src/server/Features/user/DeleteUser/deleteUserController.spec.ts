21e2dd23438dbe3b0b586a6d8bebaf5d86fe8004
karanvirsb <karanvirboparai123@gmail.com>
2023-05-21 11:31:50 -0400

    fixed removed unused

--- src/server/Features/user/DeleteUser/deleteUserController.spec.ts
+++ src/server/Features/user/DeleteUser/deleteUserController.spec.ts
@@ -1,27 +1,29 @@
-import { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import { closeDb } from "@/server/__test__/fixures/db";
 import makeFakeUser from "@/server/__test__/fixures/user";
 import userTests from "@/server/__test__/functions/user";
 
 import { addUserUC } from "../AddUser";
+import { IUser } from "../user";
 import { deleteUserC } from ".";
 
 describe("delete user controller", () => {
   jest.setTimeout(30000);
+  let user: IUser;
   beforeAll(async () => {
-    const createdUser = await userTests.addTestUserToDB({
+    user = await makeFakeUser({
       userId: "ce3735e4-b3de-48d4-853e-758c06b1a935",
     });
   });
 
   afterEach(async () => {
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "ce3735e4-b3de-48d4-853e-758c06b1a935",
     });
   });
 
   jest.setTimeout(3000);
   afterAll(async () => {
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "ce3735e4-b3de-48d4-853e-758c06b1a935",
     });
     await closeDb();
@@ -29,10 +31,7 @@ describe("delete user controller", () => {
 
   jest.setTimeout(30000);
   it("deleting user", async () => {
-    const user = await makeFakeUser({
-      userId: "ce3735e4-b3de-48d4-853e-758c06b1a935",
-    });
-    const resp = await addUserUC(user);
+    await addUserUC(user);
 
     const deletedUser = await deleteUserC({
       body: { id: "ce3735e4-b3de-48d4-853e-758c06b1a935" },

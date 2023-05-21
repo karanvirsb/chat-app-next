97b6b3211bcb08e88ad9b864258cbb269bdee5b6
karanvirsb <karanvirboparai123@gmail.com>
2023-05-21 09:26:02 -0400

    cleaned up removed supertoken

--- src/server/Features/user/controllers/edit-user.spec.ts
+++ src/server/Features/user/controllers/edit-user.spec.ts
@@ -1,40 +1,27 @@
-import { clearDb, closeDb } from "../../../../__test__/fixures/db";
-import makeFakeUser from "../../../../__test__/fixures/user";
-import makeSupertokenDb, {
-  IMakeSupertokensDb,
-} from "../../../../supertokens/data-access/supertokens-db";
-import { makeDb } from "../data-access";
-import makeUsersDb, { IMakeUsersDb } from "../data-access/users-db";
-import { addUser } from "../use-cases";
+import { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import makeFakeUser from "@/server/__test__/fixures/user";
+import userTests from "@/server/__test__/functions/user";
+
+import { addUserUC } from "../AddUser";
 import { editAnUser } from ".";
 
 describe("Edit user controller", () => {
-  let usersDb: IMakeUsersDb["returnType"];
-  const SupertokensDb: IMakeSupertokensDb["returnType"] = makeSupertokenDb({
-    makeDb,
-  });
-
   jest.setTimeout(30000);
   beforeAll(async () => {
-    const createdUser = await SupertokensDb.addUser({
-      user: {
-        user_id: "312c0878-04c3-4585-835e-c66900ccc7a1",
-        email: "randoms@gmai.com",
-        password: "123",
-        time_joined: Date.now(),
-      },
+    const createdUser = userTests.addTestUserToDB({
+      userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
-    await clearDb("usert");
-    usersDb = makeUsersDb({ makeDb });
   });
 
   afterEach(async () => {
-    await clearDb("usert");
+    await userTests.deleteTestUser({
+      userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
+    });
   });
 
   jest.setTimeout(3000);
   afterAll(async () => {
-    const deletedUser = await SupertokensDb.deleteUser({
+    await userTests.deleteTestUser({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
     await closeDb();
@@ -44,7 +31,7 @@ describe("Edit user controller", () => {
     const user = await makeFakeUser({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
-    const resp = await addUser(user);
+    const resp = await addUserUC(user);
     const edittedUser = await editAnUser({
       body: {
         id: "312c0878-04c3-4585-835e-c66900ccc7a1",
@@ -58,7 +45,6 @@ describe("Edit user controller", () => {
       query: {},
     });
 
-    console.log(edittedUser);
     expect(edittedUser.body.data.status).toBe("offline");
   });
 });

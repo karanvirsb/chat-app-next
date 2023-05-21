896e05cf3298d9dc4eb6010163ba179aac608c09
karanvirsb <karanvirboparai123@gmail.com>
2023-05-21 11:10:36 -0400

    updated removed unused code

--- src/server/Features/user/controllers/get-user.spec.ts
+++ src/server/Features/user/controllers/get-user.spec.ts
@@ -1,39 +1,38 @@
-import { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import { closeDb } from "@/server/__test__/fixures/db";
 import makeFakeUser from "@/server/__test__/fixures/user";
 import userTests from "@/server/__test__/functions/user";
 
 import { addUserUC } from "../AddUser";
-import { makeDb } from "../data-access";
-import makeUsersDb, { IMakeUsersDb } from "../data-access/users-db";
+import { IUser } from "../user";
 import { getAnUser } from ".";
 
 describe("Get user controller", () => {
-  jest.setTimeout(30000);
+  let fakeUser: IUser;
   beforeAll(async () => {
-    const createdUser = await userTests.addTestUserToDB({
+    fakeUser = await makeFakeUser({
+      userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+    });
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
   });
 
   afterEach(async () => {
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
   });
 
   jest.setTimeout(3000);
   afterAll(async () => {
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
     await closeDb();
   });
 
   it("successfully retrieve user", async () => {
-    const fakeUser = await makeFakeUser({
-      userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-    });
-    const resp = await addUserUC(fakeUser);
+    await addUserUC(fakeUser);
     const user = await getAnUser({
       body: {},
       headers: {},
@@ -43,7 +42,6 @@ describe("Get user controller", () => {
       path: "",
       query: {},
     });
-    console.log(user);
     expect(user.body.data.username).toBe(fakeUser.username);
   });
 });

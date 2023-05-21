ae8fb640e542eba8210781ed48a5adb1d0b8d830
karanvirsb <karanvirboparai123@gmail.com>
2023-05-21 09:17:10 -0400

    removed super tokens

--- src/server/Features/group/use-cases/deleteGroup.spec.ts
+++ src/server/Features/group/use-cases/deleteGroup.spec.ts
@@ -1,9 +1,8 @@
-import makeDb, { clearDb, closeDb } from "../../../../__test__/fixures/db";
-import makeFakeGroup from "../../../../__test__/fixures/group";
-import supertokens from "../../../../supertokens";
-import makeSupertokenDb from "../../../../supertokens/data-access/supertokens-db";
+import makeDb, { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import makeFakeGroup from "@/server/__test__/fixures/group";
+import userTests from "@/server/__test__/functions/user";
+
 import { moderateName } from "../../../Utilities/moderateText";
-import makeUsersDb from "../../user/data-access/users-db";
 import makeGroupDb from "../data-access/group-db";
 import makeAddGroup from "./addGroup";
 import makeDeleteGroup from "./deleteGroup";
@@ -16,52 +15,30 @@ describe("Deleting group use case", () => {
   const groupDb = makeGroupDb({ makeDb });
   const addGroup = makeAddGroup({ groupDb, handleModeration });
   const deleteGroup = makeDeleteGroup({ groupDb });
-  const SupertokensDb = makeSupertokenDb({ makeDb });
   beforeAll(async () => {
     // creating user if it does not exist
-    const userDb = makeUsersDb({ makeDb });
-    const foundUser = await userDb.findById({
-      id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    const addedUser = userTests.addTestUserToDB({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
-
-    // if user does not exist create
-    if (!foundUser.success || !foundUser.data) {
-      const addedUser = await SupertokensDb.addUser({
-        user: {
-          user_id: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
-          email: "anTest@gmai.com",
-          password: "123",
-          time_joined: Date.now(),
-        },
-      });
-      if (addedUser.success && addedUser.data) {
-        const addUser = await userDb.insert({
-          data: {
-            userId: addedUser.data.user_id,
-            status: "online",
-            username: "testering",
-          },
-        });
-      }
-    }
+    // if user does not exist creat
   });
 
   afterAll(async () => {
-    await clearDb("groupt");
-    await clearDb('"groupUsers"');
-    await SupertokensDb.deleteUser({
+    // TODO
+    // await clearDb("groupt");
+    // await clearDb('"groupUsers"');
+    await userTests.deleteTestUser({
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
     await closeDb();
   });
-
   test("SUCCESS: Delete group use case", async () => {
     const group = await makeFakeGroup();
     const res = await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
 
     const removedGroup = await deleteGroup(group.groupId);
-
-    expect(removedGroup.data?.groupName).toBe(group.groupName);
+    if (removedGroup.success)
+      expect(removedGroup.data?.groupName).toBe(group.groupName);
   });
 
   test("ERROR: remove group but missing id", async () => {

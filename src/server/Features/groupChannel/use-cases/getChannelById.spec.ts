8d2cfa144503e9f27f5d8830b15a9900929a66b2
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 10:39:54 -0400

    updated

--- src/server/Features/groupChannel/use-cases/getChannelById.spec.ts
+++ src/server/Features/groupChannel/use-cases/getChannelById.spec.ts
@@ -1,11 +1,13 @@
 import makeFakeChannel from "@/server/__test__/fixures/channel";
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import groupTests from "@/server/__test__/functions/group";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
 import makeChannelDb from "../data-access/channel-db";
+import { IGroupChannel } from "../groupChannel";
 import makeCreateChannel, { handleModerationType } from "./createChannel";
+import makeDeleteChannel from "./deleteChannel";
 import makeGetChannelById from "./getChannelById";
 
 describe("get channel by id use case", () => {
@@ -23,40 +25,48 @@ describe("get channel by id use case", () => {
     channelDb,
   });
 
+  const deleteGroupChannel = makeDeleteChannel({ channelDb });
+  let channel: IGroupChannel;
+
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({ userId: "123" });
-    const addedGroup = await groupTests.createTestGroup({
+    await userTests.addTestUserToDB({ userId: "123" });
+    await groupTests.createTestGroup({
       groupId: "123",
       userId: "123",
     });
   });
 
+  beforeEach(async () => {
+    channel = await makeFakeChannel({ groupId: "123" });
+  });
+
+  afterEach(async () => {
+    await deleteGroupChannel(channel.channelId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("group_channels");
-    const deletedUser = await userTests.deleteTestUser({ userId: "123" });
-    const deletedGroup = await groupTests.deleteTestGroup({
+    await userTests.deleteTestUser({
+      userId: "123",
+    });
+    await groupTests.deleteTestGroup({
       groupId: "123",
       userId: "123",
     });
   });
 
   test("SUCCESS: get channel by id", async () => {
-    const channel = await makeFakeChannel();
-    channel.groupId = "123";
-    const createdChannel = await createChannel(channel);
+    await createChannel(channel);
 
     const foundChannel = await getChannelById(channel.channelId);
     expect(foundChannel.data?.channelName).toBe(channel.channelName);
   });
 
   test("ERROR: channel id does not exist", async () => {
-    const channel = await makeFakeChannel();
-    channel.groupId = "123";
-    const createdChannel = await createChannel(channel);
+    await createChannel(channel);
 
     try {
-      const foundChannel = await getChannelById("");
+      await getChannelById("");
     } catch (error) {
       if (error instanceof Error)
         expect(error.message).toBe("Channel Id needs to be supplied");

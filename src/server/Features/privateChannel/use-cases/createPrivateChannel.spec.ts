aca2ca646fe69a5e142cfa1a9d9f9b7ba067aa0a
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 08:58:37 -0400

    updated added before and after test

--- src/server/Features/privateChannel/use-cases/createPrivateChannel.spec.ts
+++ src/server/Features/privateChannel/use-cases/createPrivateChannel.spec.ts
@@ -1,12 +1,14 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakePrivateChannel from "@/server/__test__/fixures/privateChannel";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
 import makePrivateChannelDb from "../data-access/privateChannel-db";
+import { IPrivateChannel } from "../privateChannel";
 import makeCreatePrivateChannel, {
   handleModerationType,
 } from "./createPrivateChannel";
+import makeDeletePrivateChannel from "./deletePrivateChannel";
 
 describe("creating private channel use case", () => {
   const handleModeration: handleModerationType = async (
@@ -20,45 +22,49 @@ describe("creating private channel use case", () => {
     privateChannelDb,
   });
 
+  const deletePrivateChannel = makeDeletePrivateChannel({ privateChannelDb });
+  let channel: IPrivateChannel;
+
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const secondUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
   });
 
+  beforeEach(async () => {
+    channel = await makeFakePrivateChannel(
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+      "312c0878-04c3-4585-835e-c66900ccc7a1"
+    );
+  });
+
+  afterEach(async () => {
+    await deletePrivateChannel(channel.channelId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("private_channels");
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedSecondUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
   });
 
   test("SUCCESS: created private channel", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-
     const createdChannel = await createPrivateChannel(channel);
 
     expect(createdChannel.data?.channelName).toBe(channel.channelName);
   });
 
   test("ERROR: channel name was not provided", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-    channel.channelName = "";
+    channel["channelName"] = "";
     try {
-      const createdChannel = await createPrivateChannel(channel);
+      await createPrivateChannel(channel);
     } catch (err) {
       if (err instanceof Error)
         expect(err.message).toBe(
@@ -68,15 +74,10 @@ describe("creating private channel use case", () => {
   });
 
   test("ERROR: user id was not provided", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-
-    channel.userId = "";
+    channel["userId"] = "";
 
     try {
-      const createdChannel = await createPrivateChannel(channel);
+      await createPrivateChannel(channel);
     } catch (err) {
       if (err instanceof Error)
         expect(err.message).toBe("User Id needs to be supplied");
@@ -84,15 +85,10 @@ describe("creating private channel use case", () => {
   });
 
   test("ERROR: friends id was not provided", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-
-    channel.friendsId = "";
+    channel["friendsId"] = "";
 
     try {
-      const createdChannel = await createPrivateChannel(channel);
+      await createPrivateChannel(channel);
     } catch (err) {
       if (err instanceof Error)
         expect(err.message).toBe("Friends Id needs to be supplied");

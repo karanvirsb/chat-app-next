f841221c69b23f8214b94834d498296b706944e8
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 10:24:50 -0400

    updated

--- src/server/Features/groupMessage/controllers/create-message.spec.ts
+++ src/server/Features/groupMessage/controllers/create-message.spec.ts
@@ -1,11 +1,13 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakeMessage from "@/server/__test__/fixures/message";
 import groupTests from "@/server/__test__/functions/group";
 import channelTests from "@/server/__test__/functions/groupChannel";
 import userTests from "@/server/__test__/functions/user";
 
 import makeMessageDb from "../data-access/message-db";
+import { IGroupMessage } from "../groupMessage";
 import makeCreateMessage from "../use-cases/createMessage";
+import makeDeleteMessage from "../use-cases/deleteMessage";
 import makeCreateMessageController from "./create-message";
 
 describe("creating a message controller", () => {
@@ -15,40 +17,48 @@ describe("creating a message controller", () => {
     createMessage,
   });
 
+  const deleteGroupMessage = makeDeleteMessage({ messageDb: messageDb });
+  let message: IGroupMessage;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedGroup = await groupTests.createTestGroup({
+    await groupTests.createTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedChannel = await channelTests.createTestChannel({
+    await channelTests.createTestChannel({
       groupId: "123",
       channelId: "123",
     });
   });
 
+  beforeEach(async () => {
+    message = await makeFakeMessage(
+      "123",
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+    );
+  });
+
+  afterEach(async () => {
+    await deleteGroupMessage(message.messageId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("group_messages");
-    const deletedChannel = await channelTests.deleteTestChannel({
+    await channelTests.deleteTestChannel({
       channelId: "123",
     });
-    const deletedGroup = await groupTests.deleteTestGroup({
+    await groupTests.deleteTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
   });
 
   test("SUCCESS: create a message", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
     const messageRequest = {
       body: {
         messageInfo: message,
@@ -62,15 +72,12 @@ describe("creating a message controller", () => {
     };
 
     const createdMessage = await createMessageController(messageRequest);
-
-    expect(createdMessage.body.data?.messageId).toBe(message.messageId);
+    if (createdMessage.body.success)
+      expect(createdMessage.body.data?.messageId).toBe(message.messageId);
   });
 
   test("ERROR: channel id missing ", async () => {
-    const message = await makeFakeMessage(
-      "",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
+    message["channelId"] = "";
     const messageRequest = {
       body: {
         messageInfo: message,
@@ -84,12 +91,14 @@ describe("creating a message controller", () => {
     };
 
     const createdMessage = await createMessageController(messageRequest);
-
-    expect(createdMessage.body.error).toBe("Channel Id needs to be supplied.");
+    if (!createdMessage.body.success)
+      expect(createdMessage.body.error).toBe(
+        "Channel Id needs to be supplied."
+      );
   });
 
   test("ERROR: user id missing ", async () => {
-    const message = await makeFakeMessage("123", "");
+    message["userId"] = "";
     const messageRequest = {
       body: {
         messageInfo: message,
@@ -103,16 +112,12 @@ describe("creating a message controller", () => {
     };
 
     const createdMessage = await createMessageController(messageRequest);
-
-    expect(createdMessage.body.error).toBe("User Id needs to be supplied.");
+    if (!createdMessage.body.success)
+      expect(createdMessage.body.error).toBe("User Id needs to be supplied.");
   });
 
   test("ERROR: text id missing ", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    message.text = "";
+    message["text"] = "";
     const messageRequest = {
       body: {
         messageInfo: message,
@@ -126,7 +131,7 @@ describe("creating a message controller", () => {
     };
 
     const createdMessage = await createMessageController(messageRequest);
-
-    expect(createdMessage.body.error).toBe("Text needs to be supplied.");
+    if (!createdMessage.body.success)
+      expect(createdMessage.body.error).toBe("Text needs to be supplied.");
   });
 });

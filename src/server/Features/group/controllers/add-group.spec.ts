57616ffacd0168920dc3b2a820ac10d2972afe86
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 11:18:36 -0400

    updated

--- src/server/Features/group/controllers/add-group.spec.ts
+++ src/server/Features/group/controllers/add-group.spec.ts
@@ -1,9 +1,11 @@
 import makeDb, { closeDb } from "@/server/__test__/fixures/db";
 import makeFakeGroup from "@/server/__test__/fixures/group";
+import groupTests from "@/server/__test__/functions/group";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
 import makeGroupDb from "../data-access/group-db";
+import { IGroup } from "../group";
 import makeAddGroup from "../use-cases/addGroup";
 import makeAddGroupController from "./add-group";
 
@@ -26,22 +28,36 @@ describe("Add group controller", () => {
   const addGroup = makeAddGroup({ groupDb, handleModeration });
   const addGroupController = makeAddGroupController({ addGroup });
 
+  let group: IGroup;
   beforeAll(async () => {
     // creating user if it does not exist
-    userTests.addTestUserToDB({
-      userId: '"cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"',
+    await userTests.addTestUserToDB({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+    // if user does not exist creat
+  });
+  beforeEach(async () => {
+    group = await makeFakeGroup();
+  });
+  afterEach(async () => {
+    await groupTests.deleteTestGroup({
+      groupId: group.groupId,
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
   });
-  // TODO after each add group, before each create group
+
   afterAll(async () => {
     await userTests.deleteTestUser({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+    groupTests.deleteTestGroup({
+      groupId: group.groupId,
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
     await closeDb();
   });
 
   test("SUCCESS: add group to the database", async () => {
-    const group = await makeFakeGroup();
     const groupRequest = {
       body: {
         groupInfo: group,
@@ -61,7 +77,6 @@ describe("Add group controller", () => {
   });
 
   test("ERROR: user Id was not given", async () => {
-    const group = await makeFakeGroup();
     const groupRequest = {
       body: {
         groupInfo: group,

4d01dc14686e87aeb92b5418bbdd8f6e3dac0d22
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 11:48:08 -0400

    updated to have usecase return

--- src/server/Features/privateMessage/use-cases/createPrivateMessage.ts
+++ src/server/Features/privateMessage/use-cases/createPrivateMessage.ts
@@ -6,28 +6,25 @@ type props = {
   privateMessageDb: IMakePrivateMessageDb["returnType"];
 };
 
-type returnData = Promise<{
-  success: boolean;
-  data: IPrivateMessage | undefined;
-  error: string;
-}>;
-
 export interface ICreatePrivateMessageUseCase {
-  createPrivateMessage: (messageInfo: IPrivateMessage) => returnData;
+  createPrivateMessage: (
+    messageInfo: IPrivateMessage
+  ) => Promise<UseCaseReturn<IPrivateMessage>>;
 }
 
 export default function makeCreatePrivateMessage({ privateMessageDb }: props) {
   return async function createPrivateMessage(
     messageInfo: IPrivateMessage
-  ): returnData {
+  ): Promise<UseCaseReturn<IPrivateMessage>> {
     const message = makePrivateMessage(messageInfo);
+    if (!message.success) return message;
 
     return privateMessageDb.createPrivateMessage({
-      privateChannelId: message.getPrivateChannelId(),
-      dateCreated: message.getDateCreated(),
-      messageId: message.getMessageId(),
-      text: message.getText(),
-      userId: message.getUserId(),
+      privateChannelId: message.data.getPrivateChannelId(),
+      dateCreated: message.data.getDateCreated(),
+      messageId: message.data.getMessageId(),
+      text: message.data.getText(),
+      userId: message.data.getUserId(),
     });
   };
 }

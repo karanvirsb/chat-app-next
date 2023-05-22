192b74ed238cb509fc6c0cdb35df43dbc8b1a1c6
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 08:48:45 -0400

    fixed

--- src/server/Features/privateMessage/controllers/update-dateModified.spec.ts
+++ src/server/Features/privateMessage/controllers/update-dateModified.spec.ts
@@ -1,11 +1,12 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakePrivateMessage from "@/server/__test__/fixures/privateMessage";
 import privateChannelTests from "@/server/__test__/functions/privateChannel";
 import userTests from "@/server/__test__/functions/user";
 
-import makePrivateChannelDb from "../../privateChannel/data-access/privateChannel-db";
 import makePrivateMessageDb from "../data-access/privateMessage-db";
+import { IPrivateMessage } from "../privateMessage";
 import makeCreatePrivateMessage from "../use-cases/createPrivateMessage";
+import makeDeletePrivateMessage from "../use-cases/deletePrivateMessage";
 import makeUpdateDateModified from "../use-cases/updateDateModified";
 import makeUpdateDateModifiedController from "./update-dateModified";
 
@@ -18,40 +19,48 @@ describe("updating private message date modified controller", () => {
     updateDateModified,
   });
 
+  const deletePrivateMessage = makeDeletePrivateMessage({ privateMessageDb });
+  let message: IPrivateMessage;
+
   jest.setTimeout(30000);
   beforeAll(async () => {
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const secondUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
-    const privateChannel = await privateChannelTests.createTestPrivateChannel({
+    await privateChannelTests.createTestPrivateChannel({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
       friendsId: "312c0878-04c3-4585-835e-c66900ccc7a1",
       channelId: "123",
     });
   });
 
+  beforeEach(async () => {
+    message = await makeFakePrivateMessage(
+      "123",
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+    );
+  });
+
+  afterEach(async () => {
+    await deletePrivateMessage(message.messageId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("private_messages");
-    const deletedPrivateChannel =
-      await privateChannelTests.deleteTestPrivateChannel({
-        channelId: "123",
-      });
-    const deletedUser = await userTests.deleteTestUser({
+    await privateChannelTests.deleteTestPrivateChannel({
+      channelId: "123",
+    });
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedSecondUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
   });
 
   test("SUCCESS: updated date modified", async () => {
-    const message = await makeFakePrivateMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
     const messageRequest = {
       body: {
         messageId: message.messageId,
@@ -65,7 +74,7 @@ describe("updating private message date modified controller", () => {
       query: {},
     };
 
-    const createdMessage = await createMessage(message);
+    await createMessage(message);
 
     const updatedMessage = await updateDateModifiedController(messageRequest);
 
@@ -75,10 +84,6 @@ describe("updating private message date modified controller", () => {
   });
 
   test("ERROR: message id missing ", async () => {
-    const message = await makeFakePrivateMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
     const messageRequest = {
       body: {
         messageId: "",
@@ -92,7 +97,7 @@ describe("updating private message date modified controller", () => {
       query: {},
     };
 
-    const createdMessage = await createMessage(message);
+    await createMessage(message);
 
     const updatedMessage = await updateDateModifiedController(messageRequest);
 
@@ -100,10 +105,6 @@ describe("updating private message date modified controller", () => {
   });
 
   test("ERROR: date modified missing ", async () => {
-    const message = await makeFakePrivateMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
     const messageRequest = {
       body: {
         messageId: message.messageId,
@@ -117,7 +118,7 @@ describe("updating private message date modified controller", () => {
       query: {},
     };
 
-    const createdMessage = await createMessage(message);
+    await createMessage(message);
     const updatedMessage = await updateDateModifiedController(messageRequest);
     expect(updatedMessage.body.error).toBe("Update Value needs to be a Date.");
   });

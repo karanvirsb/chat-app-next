863ca32227c5758566130318a04cde0da1a4bcab
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 11:14:27 -0400

    updated

--- src/server/Features/group/use-cases/removeUserFromGroup.spec.ts
+++ src/server/Features/group/use-cases/removeUserFromGroup.spec.ts
@@ -1,9 +1,11 @@
-import makeDb, { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import makeDb, { closeDb } from "@/server/__test__/fixures/db";
 import makeFakeGroup from "@/server/__test__/fixures/group";
+import groupTests from "@/server/__test__/functions/group";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
 import makeGroupDb from "../data-access/group-db";
+import { IGroup } from "../group";
 import makeAddGroup from "./addGroup";
 import makeRemoveUserFromGroup from "./removeUserFromGroup";
 
@@ -17,27 +19,37 @@ describe("Removing user from a group use case", () => {
   const addGroup = makeAddGroup({ groupDb, handleModeration });
   const removeUserFromGroup = makeRemoveUserFromGroup({ groupDb });
 
+  let group: IGroup;
   beforeAll(async () => {
     // creating user if it does not exist
-    const addedUser = userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
     // if user does not exist creat
   });
+  beforeEach(async () => {
+    group = await makeFakeGroup();
+  });
+  afterEach(async () => {
+    await groupTests.deleteTestGroup({
+      groupId: group.groupId,
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+  });
 
   afterAll(async () => {
-    // TODO
-    // // TODO await clearDb("groupt");
-    // // TODO await clearDb('"groupUsers"');
     await userTests.deleteTestUser({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+    groupTests.deleteTestGroup({
+      groupId: group.groupId,
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
     await closeDb();
   });
 
   test("SUCCESS: remove user from group", async () => {
-    const group = await makeFakeGroup();
-    const res = await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
+    await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
 
     const removedUser = await removeUserFromGroup(
       group.groupId,
@@ -48,16 +60,9 @@ describe("Removing user from a group use case", () => {
   });
 
   test("ERROR: removing user from a group but no group id", async () => {
-    const group = await makeFakeGroup();
-    const addedGroup = await addGroup(
-      group,
-      "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"
-    );
+    await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
     try {
-      const groupErr = await removeUserFromGroup(
-        "",
-        "3443c648-3323-4d6b-8830-c8a1b66a043a"
-      );
+      await removeUserFromGroup("", "3443c648-3323-4d6b-8830-c8a1b66a043a");
     } catch (err) {
       if (err instanceof Error)
         expect(err.message).toBe("Group Id needs to be supplied");
@@ -65,13 +70,9 @@ describe("Removing user from a group use case", () => {
   });
 
   test("ERROR: removing user from a group but no user id is provided", async () => {
-    const group = await makeFakeGroup();
-    const addedGroup = await addGroup(
-      group,
-      "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"
-    );
+    await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
     try {
-      const groupErr = await removeUserFromGroup(group.groupId, "");
+      await removeUserFromGroup(group.groupId, "");
     } catch (err) {
       if (err instanceof Error) {
         console.log(err.message);

a721d151a7df522ec3079222ef91b3cd521a83e7
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 09:08:10 -0400

    fixed

--- src/server/Features/privateChannel/controllers/get-privateChannelById.spec.ts
+++ src/server/Features/privateChannel/controllers/get-privateChannelById.spec.ts
@@ -1,10 +1,12 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakePrivateChannel from "@/server/__test__/fixures/privateChannel";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
 import makePrivateChannelDb from "../data-access/privateChannel-db";
+import { IPrivateChannel } from "../privateChannel";
 import makeCreatePrivateChannel from "../use-cases/createPrivateChannel";
+import makeDeletePrivateChannel from "../use-cases/deletePrivateChannel";
 import makeGetPrivateChannelById from "../use-cases/getPrivateChannelById";
 import makeGetPrivateChannelByIdController from "./get-privateChannelById";
 
@@ -36,33 +38,40 @@ describe("Get private channel by id controller", () => {
     getPrivateChannelById,
   });
 
+  const deletePrivateChannel = makeDeletePrivateChannel({ privateChannelDb });
+  let channel: IPrivateChannel;
+
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const secondUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
   });
 
+  beforeEach(async () => {
+    channel = await makeFakePrivateChannel(
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
+      "312c0878-04c3-4585-835e-c66900ccc7a1"
+    );
+  });
+
+  afterEach(async () => {
+    await deletePrivateChannel(channel.channelId);
+  });
+
   afterAll(async () => {
-    jest.setTimeout(30000);
-    // TODO await clearDb("private_channels");
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedSecondUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "312c0878-04c3-4585-835e-c66900ccc7a1",
     });
   });
 
   test("SUCCESS: get channel by id", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-
     const channelRequest = {
       body: {},
       headers: {},
@@ -73,18 +82,13 @@ describe("Get private channel by id controller", () => {
       query: {},
     };
 
-    const createdChannel = await createChannel(channel);
+    await createChannel(channel);
 
     const foundChannel = await getPrivateChannelByIdController(channelRequest);
     expect(foundChannel.body.data?.channelId).toBe(channel.channelId);
   });
 
   test("ERROR: channel id does not exist", async () => {
-    const channel = await makeFakePrivateChannel(
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
-      "312c0878-04c3-4585-835e-c66900ccc7a1"
-    );
-
     const channelRequest = {
       body: {},
       headers: {},
@@ -95,7 +99,7 @@ describe("Get private channel by id controller", () => {
       query: {},
     };
 
-    const createdChannel = await createChannel(channel);
+    await createChannel(channel);
 
     const foundChannel = await getPrivateChannelByIdController(channelRequest);
     expect(foundChannel.body.error).toBe("Channel Id needs to be supplied");

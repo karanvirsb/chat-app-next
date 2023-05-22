ba74c85a29dddc937fe85d7f2ae8bfc8012e1971
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 10:11:48 -0400

    updated

--- src/server/Features/groupMessage/use-cases/updateMessageText.spec.ts
+++ src/server/Features/groupMessage/use-cases/updateMessageText.spec.ts
@@ -1,11 +1,13 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakeMessage from "@/server/__test__/fixures/message";
 import groupTests from "@/server/__test__/functions/group";
 import channelTests from "@/server/__test__/functions/groupChannel";
 import userTests from "@/server/__test__/functions/user";
 
 import makeMessageDb from "../data-access/message-db";
+import { IGroupMessage } from "../groupMessage";
 import makeCreateMessage from "./createMessage";
+import makeDeleteMessage from "./deleteMessage";
 import makeUpdateMessageText from "./updateMessageText";
 
 describe("updating message text use case", () => {
@@ -14,41 +16,50 @@ describe("updating message text use case", () => {
   const createMessage = makeCreateMessage({ messageDb });
   const updateMessageText = makeUpdateMessageText({ messageDb });
 
+  const deleteGroupMessage = makeDeleteMessage({ messageDb });
+  let message: IGroupMessage;
+
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedGroup = await groupTests.createTestGroup({
+    await groupTests.createTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedChannel = await channelTests.createTestChannel({
+    await channelTests.createTestChannel({
       groupId: "123",
       channelId: "123",
     });
   });
 
+  beforeEach(async () => {
+    message = await makeFakeMessage(
+      "123",
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+    );
+  });
+
+  afterEach(async () => {
+    await deleteGroupMessage(message.messageId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("group_messages");
-    const deletedChannel = await channelTests.deleteTestChannel({
+    await channelTests.deleteTestChannel({
       channelId: "123",
     });
-    const deletedGroup = await groupTests.deleteTestGroup({
+    await groupTests.deleteTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
   });
 
   test("SUCCESS: updating a message text", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    const insertedMessage = await createMessage(message);
+    await createMessage(message);
 
     const updatedMessage = await updateMessageText(
       message.messageId,
@@ -59,14 +70,10 @@ describe("updating message text use case", () => {
   });
 
   test("ERROR: missing message id ", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    const insertedMessage = await createMessage(message);
+    await createMessage(message);
 
     try {
-      const updateMessage = await updateMessageText("", "Coding");
+      await updateMessageText("", "Coding");
     } catch (error) {
       if (error instanceof Error)
         expect(error.message).toBe("Message Id needs to be supplied.");
@@ -74,14 +81,10 @@ describe("updating message text use case", () => {
   });
 
   test("ERROR: missing text ", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    const insertedMessage = await createMessage(message);
+    await createMessage(message);
 
     try {
-      const updateMessage = await updateMessageText(message.messageId, "");
+      await updateMessageText(message.messageId, "");
     } catch (error) {
       if (error instanceof Error)
         expect(error.message).toBe("Update Value needs to be supplied.");

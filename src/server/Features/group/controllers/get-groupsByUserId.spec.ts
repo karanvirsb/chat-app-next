bb7c239cfc08b44c8b38dbad7ddac4fea72cf310
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 11:21:51 -0400

    updated

--- src/server/Features/group/controllers/get-groupsByUserId.spec.ts
+++ src/server/Features/group/controllers/get-groupsByUserId.spec.ts
@@ -1,10 +1,11 @@
-import makeDb, { clearDb, closeDb } from "@/server/__test__/fixures/db";
+import makeDb, { closeDb } from "@/server/__test__/fixures/db";
 import makeFakeGroup from "@/server/__test__/fixures/group";
+import groupTests from "@/server/__test__/functions/group";
 import userTests from "@/server/__test__/functions/user";
 
 import { moderateName } from "../../../Utilities/moderateText";
-import makeUsersDb from "../../user/data-access/users-db";
 import makeGroupDb from "../data-access/group-db";
+import { IGroup } from "../group";
 import makeAddGroup from "../use-cases/addGroup";
 import makeGetGroupsByUserId from "../use-cases/getGroupsByUserId";
 import makeGetGroupsByUserIdController from "./get-groupsByUserId";
@@ -31,24 +32,36 @@ describe("get groups by user Id controller", () => {
     getGroupsByUserId,
   });
 
+  let group: IGroup;
   beforeAll(async () => {
-    const addedUser = userTests.addTestUserToDB({
+    // creating user if it does not exist
+    await userTests.addTestUserToDB({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+    // if user does not exist creat
+  });
+  beforeEach(async () => {
+    group = await makeFakeGroup();
+  });
+  afterEach(async () => {
+    await groupTests.deleteTestGroup({
+      groupId: group.groupId,
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
   });
 
   afterAll(async () => {
-    // TODO
-    // // TODO await clearDb("groupt");
-    // // TODO await clearDb('"groupUsers"');
     await userTests.deleteTestUser({
+      userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
+    });
+    groupTests.deleteTestGroup({
+      groupId: group.groupId,
       userId: "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc",
     });
     await closeDb();
   });
 
   test("SUCCESS: get groups by user id", async () => {
-    const group = await makeFakeGroup();
     const groupRequest = {
       body: {},
       headers: { "Content-Type": "application/json" },
@@ -59,17 +72,13 @@ describe("get groups by user Id controller", () => {
       query: {},
     };
 
-    const addedGroup = await addGroup(
-      group,
-      "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"
-    );
+    await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
     const foundGroups = await getGroupsByUserIdController(groupRequest);
     if (foundGroups.body.data)
       expect(foundGroups.body.data[0].groupId).toBe(group.groupId);
   });
 
   test("ERROR: missing user id", async () => {
-    const group = await makeFakeGroup();
     const groupRequest = {
       body: {},
       headers: { "Content-Type": "application/json" },
@@ -80,10 +89,7 @@ describe("get groups by user Id controller", () => {
       query: {},
     };
 
-    const addedGroup = await addGroup(
-      group,
-      "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc"
-    );
+    await addGroup(group, "cc7d98b5-6f88-4ca5-87e2-435d1546f1fc");
     const foundGroups = await getGroupsByUserIdController(groupRequest);
     expect(foundGroups.body.error).toBe("User Id needs to be supplied.");
   });

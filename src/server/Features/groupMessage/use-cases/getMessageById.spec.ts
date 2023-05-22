df2214bb0c19c940aa54d5f0b81278d3d0aa05bb
karanvirsb <karanvirboparai123@gmail.com>
2023-05-22 10:06:31 -0400

    updated

--- src/server/Features/groupMessage/use-cases/getMessageById.spec.ts
+++ src/server/Features/groupMessage/use-cases/getMessageById.spec.ts
@@ -1,11 +1,13 @@
-import makeDb, { clearDb } from "@/server/__test__/fixures/db";
+import makeDb from "@/server/__test__/fixures/db";
 import makeFakeMessage from "@/server/__test__/fixures/message";
 import groupTests from "@/server/__test__/functions/group";
 import channelTests from "@/server/__test__/functions/groupChannel";
 import userTests from "@/server/__test__/functions/user";
 
 import makeMessageDb from "../data-access/message-db";
+import { IGroupMessage } from "../groupMessage";
 import makeCreateMessage from "./createMessage";
+import makeDeleteMessage from "./deleteMessage";
 import makeGetMessageById from "./getMessageById";
 
 describe("Get messages by id use case", () => {
@@ -13,41 +15,49 @@ describe("Get messages by id use case", () => {
   const createMessage = makeCreateMessage({ messageDb });
   const getMessageById = makeGetMessageById({ messageDb });
 
+  const deleteGroupMessage = makeDeleteMessage({ messageDb });
+  let message: IGroupMessage;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    const addedUser = await userTests.addTestUserToDB({
+    await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedGroup = await groupTests.createTestGroup({
+    await groupTests.createTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const addedChannel = await channelTests.createTestChannel({
+    await channelTests.createTestChannel({
       groupId: "123",
       channelId: "123",
     });
   });
 
+  beforeEach(async () => {
+    message = await makeFakeMessage(
+      "123",
+      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
+    );
+  });
+
+  afterEach(async () => {
+    await deleteGroupMessage(message.messageId);
+  });
+
   afterAll(async () => {
-    // TODO await clearDb("group_messages");
-    const deletedChannel = await channelTests.deleteTestChannel({
+    await channelTests.deleteTestChannel({
       channelId: "123",
     });
-    const deletedGroup = await groupTests.deleteTestGroup({
+    await groupTests.deleteTestGroup({
       groupId: "123",
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
-    const deletedUser = await userTests.deleteTestUser({
+    await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
     });
   });
 
   test("SUCCESS: getting a message", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    const insertedMessage = await createMessage(message);
+    await createMessage(message);
 
     const foundMessage = await getMessageById(message.messageId);
 
@@ -55,14 +65,10 @@ describe("Get messages by id use case", () => {
   });
 
   test("ERROR: missing message id ", async () => {
-    const message = await makeFakeMessage(
-      "123",
-      "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
-    );
-    const insertedMessage = await createMessage(message);
+    await createMessage(message);
 
     try {
-      const foundMessage = await getMessageById("");
+      await getMessageById("");
     } catch (error) {
       if (error instanceof Error)
         expect(error.message).toBe("Message Id needs to be supplied.");

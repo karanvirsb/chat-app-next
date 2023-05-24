18842510f98227d410b5bbff5dd4a709f5218e8b
karanvirsb <karanvirboparai123@gmail.com>
2023-05-24 15:06:36 -0400

    updated tests

--- src/server/Features/group/group.spec.ts
+++ src/server/Features/group/group.spec.ts
@@ -37,15 +37,46 @@ describe("Group creation test", () => {
   test("Group Name must be a certain length", () => {
     const result = buildGroup({ ...group, groupName: "bo" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IGroup>).flatten().fieldErrors;
+        console.log(err);
+        expect(err.groupName?.join("")).toBe(
+          "Group name must be at least 3 characters long"
+        );
+      }
+    }
   });
 
   test("Group id is required", () => {
     const result = buildGroup({ ...group, groupId: "" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IGroup>).flatten().fieldErrors;
+        console.log(err);
+        expect(err.groupId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 
   test("Group requires an invite code", () => {
     const result = buildGroup({ ...group, inviteCode: "" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IGroup>).flatten().fieldErrors;
+        console.log(err);
+        expect(err.inviteCode?.join("")).toBe(
+          "String must contain at least 7 character(s)"
+        );
+      }
+    }
   });
 });

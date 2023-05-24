469818527dff5600e3f80759d326117240251f42
karanvirsb <karanvirboparai123@gmail.com>
2023-05-24 12:40:52 -0400

    added zod testing

--- src/server/Features/privateMessage/privateMessage.ts
+++ src/server/Features/privateMessage/privateMessage.ts
@@ -42,19 +42,33 @@ export default function buildPrivateMessage({ Id, sanitizeText }: props) {
     userId,
     privateChannelId,
   }: IPrivateMessage): EntityReturn<IPrivateMessage> {
-    if (!text) throw new Error("Text needs to be supplied.");
+    // if (!text) throw new Error("Text needs to be supplied.");
     const sanitizedText = sanitizeText(text);
 
-    if (sanitizedText.length < 1)
-      throw new Error("Enter valid characters to send a message.");
+    // if (sanitizedText.length < 1)
+    //   throw new Error("Enter valid characters to send a message.");
 
-    if (sanitizedText.length < 1 || sanitizedText.length > 200)
-      throw new Error("Messages can only be between 1 and 200 characters long");
+    // if (sanitizedText.length < 1 || sanitizedText.length > 200)
+    //   throw new Error("Messages can only be between 1 and 200 characters long");
 
-    if (!userId) throw new Error("User Id needs to be supplied.");
-    if (!dateCreated) throw new Error("Date Created needs to be supplied.");
-    if (!messageId) throw new Error("Message Id needs to be supplied.");
-    if (!privateChannelId) throw new Error("Channel Id needs to be supplied.");
+    // if (!userId) throw new Error("User Id needs to be supplied.");
+    // if (!dateCreated) throw new Error("Date Created needs to be supplied.");
+    // if (!messageId) throw new Error("Message Id needs to be supplied.");
+    // if (!privateChannelId) throw new Error("Channel Id needs to be supplied.");
+
+    const result = PrivateMessageSchema.safeParse({
+      messageId,
+      dateCreated,
+      dateModified,
+      replyTo,
+      text: sanitizedText,
+      userId,
+      privateChannelId,
+    });
+
+    if (!result.success) {
+      return { success: false, error: result.error };
+    }
 
     // replace any ' with a '' to escape
     const newMessage = sanitizedText.replace(/'/g, "\\'");

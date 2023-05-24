355a7e61b2eca7b4a7ea3eff2ab6229f52215e0a
karanvirsb <karanvirboparai123@gmail.com>
2023-05-24 14:39:20 -0400

    added zod testing

--- src/server/Features/friends/friends.ts
+++ src/server/Features/friends/friends.ts
@@ -16,10 +16,17 @@ export default function buildFriends() {
     friendId,
     dateAdded = new Date(),
   }: IFriends): EntityReturn<IFriends> {
-    if (!userId) throw new Error("User Id needs to be supplied");
-    if (!friendId) throw new Error("Friends Id needs to be supplied");
-    if (!dateAdded || Number.isNaN(dateAdded.getTime()))
-      throw new Error("Date needs to be supplied");
+    // if (!userId) throw new Error("User Id needs to be supplied");
+    // if (!friendId) throw new Error("Friends Id needs to be supplied");
+    // if (!dateAdded || Number.isNaN(dateAdded.getTime()))
+    //   throw new Error("Date needs to be supplied");
+
+    const result = FriendsSchema.safeParse({ userId, friendId, dateAdded });
+
+    if (!result.success) {
+      return result;
+    }
+
     return {
       success: true,
       data: Object.freeze({

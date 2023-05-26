8cb7d6fc9e6f1c1a35c0a2d071e08756417c2b9f
karanvirsb <karanvirboparai123@gmail.com>
2023-05-26 10:39:18 -0400

    added checks

--- src/server/Features/privateChannel/privateChannel.spec.ts
+++ src/server/Features/privateChannel/privateChannel.spec.ts
@@ -1,3 +1,5 @@
+import { ZodError } from "zod";
+
 import makePrivateChannel from ".";
 import { IPrivateChannel } from "./privateChannel";
 
@@ -21,25 +23,82 @@ describe("Channel test", () => {
       channelName: "<html></html>",
     });
     expect(result.success).toBeFalsy();
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IPrivateChannel>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.channelName?.join("")).toBe(
+          "Channel name has to be atleast 3 characters."
+        );
+      }
+    }
   });
 
   test("ERROR: channel name should be between 3-50", () => {
-    const result = makePrivateChannel({ ...channelData, channelName: "12" });
+    const result = makePrivateChannel({
+      ...channelData,
+      channelName:
+        "fjsdklfjlkdjflkejrlkjsljkfjisldfilejiljtrijljsflkjflkjaelitjlijsifjslkjeif",
+    });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IPrivateChannel>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.channelName?.join("")).toBe(
+          "Channel name cannot be more than 50 characters"
+        );
+      }
+    }
   });
 
   test("ERROR: user id does not exist", () => {
     const result = makePrivateChannel({ ...channelData, userId: "" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IPrivateChannel>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.userId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 
   test("ERROR: friends id is needed", () => {
     const result = makePrivateChannel({ ...channelData, friendsId: "" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IPrivateChannel>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.friendsId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 
   test("ERROR: channel id is needed", () => {
     const result = makePrivateChannel({ ...channelData, channelId: "" });
     expect(result.success).toBeFalsy();
+
+    if (!result.success) {
+      expect(result.error).toBeInstanceOf(ZodError);
+      if (result.error instanceof ZodError) {
+        const err = (result.error as ZodError<IPrivateChannel>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.channelId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 });

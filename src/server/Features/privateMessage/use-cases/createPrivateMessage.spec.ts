b28da5dcb1e35c70a2fe704b376e62f4d9fe50b2
karanvirsb <karanvirboparai123@gmail.com>
2023-05-26 17:30:49 -0400

    added fix

--- src/server/Features/privateMessage/use-cases/createPrivateMessage.spec.ts
+++ src/server/Features/privateMessage/use-cases/createPrivateMessage.spec.ts
@@ -2,6 +2,7 @@ import makeDb from "@/server/__test__/fixures/db";
 import makeFakePrivateMessage from "@/server/__test__/fixures/privateMessage";
 import privateChannelTests from "@/server/__test__/functions/privateChannel";
 import userTests from "@/server/__test__/functions/user";
+import id from "@/server/Utilities/id";
 
 import makePrivateMessageDb from "../data-access/privateMessage-db";
 import { IPrivateMessage } from "../privateMessage";
@@ -13,7 +14,7 @@ describe("Create private message use case", () => {
   const createMessage = makeCreatePrivateMessage({ privateMessageDb });
   const deleteMessage = makeDeletePrivateMessage({ privateMessageDb });
   let message: IPrivateMessage;
-
+  const channelUUID = id.makeId();
   beforeAll(async () => {
     await userTests.addTestUserToDB({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
@@ -24,13 +25,13 @@ describe("Create private message use case", () => {
     await privateChannelTests.createTestPrivateChannel({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
       friendsId: "312c0878-04c3-4585-835e-c66900ccc7a1",
-      channelId: "123",
+      channelId: channelUUID,
     });
   });
 
   beforeEach(async () => {
     message = await makeFakePrivateMessage(
-      "123",
+      channelUUID,
       "5c0fc896-1af1-4c26-b917-550ac5eefa9e"
     );
   });
@@ -43,7 +44,7 @@ describe("Create private message use case", () => {
     // TODO await clearDb("private_messages");
 
     await privateChannelTests.deleteTestPrivateChannel({
-      channelId: "123",
+      channelId: channelUUID,
     });
     await userTests.deleteTestUser({
       userId: "5c0fc896-1af1-4c26-b917-550ac5eefa9e",
@@ -55,37 +56,8 @@ describe("Create private message use case", () => {
 
   test("SUCCESS: creating a message", async () => {
     const insertedMessage = await createMessage(message);
-
-    expect(insertedMessage.data?.messageId).toBe(message.messageId);
-  });
-
-  test("ERROR: channelId not given", async () => {
-    try {
-      message.privateChannelId = "";
-      await createMessage(message);
-    } catch (error) {
-      if (error instanceof Error)
-        expect(error.message).toBe("Channel Id needs to be supplied.");
-    }
-  });
-
-  test("ERROR: text not given", async () => {
-    try {
-      message.text = "";
-      await createMessage(message);
-    } catch (error) {
-      if (error instanceof Error)
-        expect(error.message).toBe("Text needs to be supplied.");
-    }
-  });
-
-  test("ERROR: user id not given", async () => {
-    try {
-      message.userId = "";
-      await createMessage(message);
-    } catch (error) {
-      if (error instanceof Error)
-        expect(error.message).toBe("User Id needs to be supplied.");
-    }
+    expect(insertedMessage.success).toBeTruthy();
+    if (insertedMessage.success)
+      expect(insertedMessage.data?.messageId).toBe(message.messageId);
   });
 });

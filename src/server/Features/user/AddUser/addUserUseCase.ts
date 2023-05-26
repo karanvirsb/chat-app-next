6b27e17f25d8df235eba55ea8f20aceaeab83989
karanvirsb <karanvirboparai123@gmail.com>
2023-05-26 17:17:21 -0400

    added proper check

--- src/server/Features/user/AddUser/addUserUseCase.ts
+++ src/server/Features/user/AddUser/addUserUseCase.ts
@@ -8,26 +8,25 @@ type props = {
   handleModeration: handleModerationType;
 };
 
-type returnData = Promise<{
-  success: boolean;
-  data: IUser | undefined;
-  error: string;
-}>;
-
 export interface IAddUserUseCase {
-  addUser: (user: IUser) => Promise<returnData>;
+  addUser: (user: IUser) => Promise<UseCaseReturn<IUser>>;
 }
 
 export default function makeAddUser({ usersDb, handleModeration }: props) {
-  return async function addUser(userInfo: IUser): Promise<returnData> {
-    const user = makeUser({ ...userInfo });
-    const exists = await usersDb.findByUsername(user.getUsername());
+  return async function addUser(
+    userInfo: IUser
+  ): Promise<UseCaseReturn<IUser>> {
+    const user = makeUser(userInfo);
+    if (!user.success) {
+      return user;
+    }
+    const exists = await usersDb.findByUsername(user.data.getUsername());
 
     if (exists.success && exists.data !== undefined) {
       throw new Error("User already exists");
     }
 
-    const moderated = await handleModeration(user.getUsername());
+    const moderated = await handleModeration(user.data.getUsername());
 
     if (moderated) {
       throw new Error("Username contains profanity");
@@ -39,10 +38,10 @@ export default function makeAddUser({ usersDb, handleModeration }: props) {
 
     return await usersDb.insert({
       data: {
-        password: user.getPassword(),
-        status: user.getStatus(),
-        userId: user.getUserId(),
-        username: user.getUsername(),
+        password: user.data.getPassword(),
+        status: user.data.getStatus(),
+        userId: user.data.getUserId(),
+        username: user.data.getUsername(),
       },
     });
   };

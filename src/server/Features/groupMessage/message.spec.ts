339cfd365e09236ad890890ff2235a889c174336
karanvirsb <karanvirboparai123@gmail.com>
2023-05-26 10:28:33 -0400

    added checks

--- src/server/Features/groupMessage/message.spec.ts
+++ src/server/Features/groupMessage/message.spec.ts
@@ -1,7 +1,10 @@
+import { ZodError } from "zod";
+
 import makeFakeMessage from "@/server/__test__/fixures/message";
 import id from "@/server/Utilities/id";
 
 import makeMessage from ".";
+import { IGroupMessage } from "./groupMessage";
 describe("message tests", () => {
   test("SUCCESS: message created", async () => {
     const message = makeMessage(
@@ -15,9 +18,17 @@ describe("message tests", () => {
     fakeMessage.text = "<html></html>";
     const message = makeMessage(fakeMessage);
     expect(message.success).toBeFalsy();
-    // expect().toThrow(
-    //   "Enter valid characters to send a message."
-    // );
+    if (!message.success) {
+      expect(message.error).toBeInstanceOf(ZodError);
+      if (message.error instanceof ZodError) {
+        const err = (message.error as ZodError<IGroupMessage>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.text?.join("")).toBe(
+          "Text must be at least 3 characters long"
+        );
+      }
+    }
   });
 
   test("ERROR: text needs to 1-200 characters long", async () => {
@@ -26,9 +37,17 @@ describe("message tests", () => {
       "sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis";
     const message = makeMessage(fakeMessage);
     expect(message.success).toBeFalsy();
-    // expect(() => makeMessage(fakeMessage)).toThrow(
-    //   "Messages can only be between 1 and 200 characters long"
-    // );
+    if (!message.success) {
+      expect(message.error).toBeInstanceOf(ZodError);
+      if (message.error instanceof ZodError) {
+        const err = (message.error as ZodError<IGroupMessage>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.text?.join("")).toBe(
+          "Text must be less than 200 characters long"
+        );
+      }
+    }
   });
 
   test("ERROR: missing user id", async () => {
@@ -36,9 +55,15 @@ describe("message tests", () => {
 
     const message = makeMessage(fakeMessage);
     expect(message.success).toBeFalsy();
-    // expect(() => makeMessage(fakeMessage)).toThrow(
-    //   "User Id needs to be supplied"
-    // );
+    if (!message.success) {
+      expect(message.error).toBeInstanceOf(ZodError);
+      if (message.error instanceof ZodError) {
+        const err = (message.error as ZodError<IGroupMessage>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.userId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 
   test("ERROR: missing message id", async () => {
@@ -46,17 +71,20 @@ describe("message tests", () => {
     fakeMessage.messageId = "";
     const message = makeMessage(fakeMessage);
     expect(message.success).toBeTruthy();
-    // expect(() => makeMessage(fakeMessage)).toThrow(
-    //   "Message Id needs to be supplied"
-    // );
   });
 
   test("ERROR: missing channel id", async () => {
     const fakeMessage = await makeFakeMessage("", id.makeId());
     const message = makeMessage(fakeMessage);
     expect(message.success).toBeFalsy();
-    // expect(() => makeMessage(fakeMessage)).toThrow(
-    //   "Channel Id needs to be supplied"
-    // );
+    if (!message.success) {
+      expect(message.error).toBeInstanceOf(ZodError);
+      if (message.error instanceof ZodError) {
+        const err = (message.error as ZodError<IGroupMessage>).flatten()
+          .fieldErrors;
+        console.log(err);
+        expect(err.channelId?.join("")).toBe("Invalid uuid");
+      }
+    }
   });
 });
